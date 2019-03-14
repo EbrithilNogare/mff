d22fddb12802d35c7418abf2b0c5c578484dbfd3
David Nápravník <david.napravnik@bimproject.cz>
2019-03-15 00:38:42 +0100

    FP

--- LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
+++ LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
@@ -13,8 +13,7 @@ namespace floatingPoint
 
             var f1 = new Fixed<Q24_8>(3);
             Console.WriteLine($"3: {f1}");
-
-            var a = new Q24_8(8);
+            
 
 
 
@@ -23,14 +22,12 @@ namespace floatingPoint
             Console.ReadKey();
         }
     }
-    class Fixed<T> where T : new()
+    class Fixed<T> where T:Q
     {
         T number;
-        public Fixed(int integer)
+        public Fixed(int number)
         {
-            var instance = Activator.CreateInstance(typeof(T),
-                  new object[] { int }) as T;
-            number = new instance(integer);
+            this.number = (T)number;
         }
 
         public T Add() {
@@ -42,41 +39,82 @@ namespace floatingPoint
             return number.ToString();
         }
     }
+
+    abstract class Q
+    {
+        public static explicit operator Q(int integer) {
+            return (Q)integer;
+        }
+        abstract public override string ToString();
+    }
     
-    struct Q24_8
+    class Q24_8 : Q
     {
         private byte i1, i2, i3, f1;
-        public Q24_8(int integer)
+        public Q24_8(byte i1, byte i2, byte i3, byte f1)
         {
-            i1 = (byte)(integer & 0xFF);
-            i2 = (byte)(integer >> 8);
-            i3 = (byte)(integer >> 16);
-            f1 = (byte)(0);
+            this.i1 = i1;
+            this.i2 = i2;
+            this.i3 = i3;
+            this.f1 = f1;
         }
         public static explicit operator Q24_8(int integer)
         {
-            return new Q24_8(integer);
+            byte i1 = (byte)(integer & 0xFF);
+            byte i2 = (byte)(integer >> 8);
+            byte i3 = (byte)(integer >> 16);
+            byte f1 = (byte)(0);
+            return new Q24_8(i1, i2, i3, f1);
+        }
+        public override string ToString()
+        {
+            return String.Format(i1 + i2 + i3 + "." + f1);
         }
     }
-    /*/
-    struct Q16_16
+    class Q16_16 : Q
     {
         private byte i1, i2, f1, f2;
-        public Q16_16(int integer)
+        public Q16_16(byte i1, byte i2, byte f1, byte f2)
         {
-            i1 = (byte)(integer & 0xFF);
-            i2 = (byte)(integer >> 8);
-            f1 = f2 = (byte)(0);
+            this.i1 = i1;
+            this.i2 = i2;
+            this.f1 = f1;
+            this.f2 = f2;
+        }
+        public static explicit operator Q16_16(int integer)
+        {
+            byte i1 = (byte)(integer & 0xFF);
+            byte i2 = (byte)(integer >> 8);
+            byte f1 = (byte)(0);
+            byte f2 = (byte)(0);
+            return new Q16_16(i1, i2, f1, f2);
+        }        
+        public override string ToString()
+        {
+            return String.Format(i1 + i2 + "." + f1 + f2);
         }
     }
-    struct Q8_24
+    class Q8_24 : Q
     {
         private byte i1, f1, f2, f3;
-        public Q8_24(int integer)
+        public Q8_24(byte i1, byte f1, byte f2, byte f3)
         {
-            i1 = (byte)(integer & 0xFF);
-            f1 = f2 = f3 = (byte)(0);
+            this.i1 = i1;
+            this.f1 = f1;
+            this.f2 = f2;
+            this.f3 = f3;
+        }
+        public static explicit operator Q8_24(int integer)
+        {
+            byte i1 = (byte)(integer & 0xFF);
+            byte f1 = (byte)(0);
+            byte f2 = (byte)(0);
+            byte f3 = (byte)(0);
+            return new Q8_24(i1, f1, f2, f3);
+        }
+        public override string ToString()
+        {
+            return String.Format(i1 + "." + f1 + f2 + f3);
         }
     }
-    /*/
 }
