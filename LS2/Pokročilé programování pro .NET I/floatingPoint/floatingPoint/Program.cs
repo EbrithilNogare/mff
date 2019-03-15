bbb2307db182e3af920bd3454910e2ac6506d67f
David Nápravník <ebrithil@nogare.cz>
2019-03-15 11:04:32 +0100

    kag

--- LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
+++ LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
@@ -10,47 +10,47 @@ namespace floatingPoint
     {
         static void Main(string[] args)
         {
-
             var f1 = new Fixed<Q24_8>(3);
             Console.WriteLine($"3: {f1}");
-            
 
-
-
-
+            var f2 = new Fixed<Q24_8>(2);
+            var f3 = f1.Add(f2);
+            Console.WriteLine($"3 + 2: {f3}");
 
             Console.ReadKey();
         }
     }
-    class Fixed<T> where T:Q
+    class Fixed<T> where T : Q
     {
-        T number;
-        public Fixed(int number)
+        private T t;
+        public Fixed(T t)
         {
-            this.number = (T)number;
+            this.t = t;
         }
 
-        public T Add() {
-            throw new NotImplementedException();
+        public Q24_8 Add(Fixed<Q24_8> n)
+        {
+            var nn = n.t;
+            Q24_8 tt = t;
+            Int32 a = BitConverter.ToInt32(new byte[] { tt.i1, tt.i2, tt.i3, tt.f1, }, 0);
+            Int32 b = BitConverter.ToInt32(new byte[] { nn.i1, nn.i2, nn.i3, nn.f1, }, 0);
+            return (a + b);
         }
 
         public override string ToString()
         {
-            return number.ToString();
+            return t.ToString();
         }
+
     }
 
-    abstract class Q
-    {
-        public static explicit operator Q(int integer) {
-            return (Q)integer;
-        }
-        abstract public override string ToString();
+    abstract class Q {
+
     }
     
-    class Q24_8 : Q
+    class Q24_8:Q
     {
-        private byte i1, i2, i3, f1;
+        public byte i1, i2, i3, f1;
         public Q24_8(byte i1, byte i2, byte i3, byte f1)
         {
             this.i1 = i1;
@@ -58,19 +58,20 @@ namespace floatingPoint
             this.i3 = i3;
             this.f1 = f1;
         }
-        public static explicit operator Q24_8(int integer)
+        public static implicit operator Q24_8(int integer)
         {
-            byte i1 = (byte)(integer & 0xFF);
+            byte i3 = (byte)(integer & 0xFF);
             byte i2 = (byte)(integer >> 8);
-            byte i3 = (byte)(integer >> 16);
+            byte i1 = (byte)(integer >> 16);
             byte f1 = (byte)(0);
             return new Q24_8(i1, i2, i3, f1);
         }
         public override string ToString()
         {
-            return String.Format(i1 + i2 + i3 + "." + f1);
+            return String.Format(i1*16 + i2*8 + i3 + "." + f1);
         }
     }
+    /*/
     class Q16_16 : Q
     {
         private byte i1, i2, f1, f2;
@@ -116,5 +117,5 @@ namespace floatingPoint
         {
             return String.Format(i1 + "." + f1 + f2 + f3);
         }
-    }
+    }/**/
 }
