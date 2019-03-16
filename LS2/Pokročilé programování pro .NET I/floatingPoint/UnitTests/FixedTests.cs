ba77c1b209c67896968883c4b2fe5c94b4d555d6
David Nápravník <ebrithilnogare@gmail.com>
2019-03-16 19:36:30 +0100

    FP

---
+++ LS2/Pokročilé programování pro .NET I/floatingPoint/UnitTests/FixedTests.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using floatingPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class FixedQ24_8
+    {
+        [TestMethod()]
+        public void Init()
+        {
+            var f1 = new Fixed<Q24_8>(3);
+            Assert.AreEqual("3", f1.ToString());
+        }
+        [TestMethod()]
+        public void Add()
+        {
+            var f1 = new Fixed<Q24_8>(3);
+            var f2 = new Fixed<Q24_8>(2);
+            var f3 = f1.Add(f2);
+            Assert.AreEqual("5", f3.ToString());
+        }
+        [TestMethod()]
+        public void Subtract()
+        {
+            var f1 = new Fixed<Q24_8>(3);
+            var f2 = new Fixed<Q24_8>(2);
+            var f3 = f1.Subtract(f2);
+            Assert.AreEqual("1", f3.ToString());
+        }
+        [TestMethod()]
+        public void Multiply()
+        {
+            var f1 = new Fixed<Q24_8>(3);
+            var f2 = new Fixed<Q24_8>(2);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("6", f3.ToString());
+        }
+        [TestMethod()]
+        public void MultiplyHigher()
+        {
+            var f1 = new Fixed<Q24_8>(19);
+            var f2 = new Fixed<Q24_8>(13);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("247", f3.ToString());
+        }
+        [TestMethod()]
+        public void Divide()
+        {
+            var f1 = new Fixed<Q24_8>(3);
+            var f2 = new Fixed<Q24_8>(2);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)1.5).ToString(), f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher()
+        {
+            var f1 = new Fixed<Q24_8>(248);
+            var f2 = new Fixed<Q24_8>(10);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)24.796875).ToString(), f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher2()
+        {
+            var f1 = new Fixed<Q24_8>(625);
+            var f2 = new Fixed<Q24_8>(1000);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)0.625).ToString(), f3.ToString());
+        }
+
+    }
+
+    [TestClass()]
+    public class FixedQ16_16
+    {
+        [TestMethod()]
+        public void Init()
+        {
+            var f1 = new Fixed<Q16_16>(3);
+            Assert.AreEqual("3", f1.ToString());
+        }
+        [TestMethod()]
+        public void Add()
+        {
+            var f1 = new Fixed<Q16_16>(3);
+            var f2 = new Fixed<Q16_16>(2);
+            var f3 = f1.Add(f2);
+            Assert.AreEqual("5", f3.ToString());
+        }
+        [TestMethod()]
+        public void Subtract()
+        {
+            var f1 = new Fixed<Q16_16>(3);
+            var f2 = new Fixed<Q16_16>(2);
+            var f3 = f1.Subtract(f2);
+            Assert.AreEqual("1", f3.ToString());
+        }
+        [TestMethod()]
+        public void Multiply()
+        {
+            var f1 = new Fixed<Q16_16>(3);
+            var f2 = new Fixed<Q16_16>(2);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("6", f3.ToString());
+        }
+        [TestMethod()]
+        public void MultiplyHigher()
+        {
+            var f1 = new Fixed<Q16_16>(19);
+            var f2 = new Fixed<Q16_16>(13);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("247", f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher()
+        {
+            var f1 = new Fixed<Q16_16>(248);
+            var f2 = new Fixed<Q16_16>(10);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)24.7999877929688).ToString(), f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher2()
+        {
+            var f1 = new Fixed<Q16_16>(625);
+            var f2 = new Fixed<Q16_16>(1000);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)0.625).ToString(), f3.ToString());
+        }
+
+    }
+
+    [TestClass()]
+    public class FixedQ8_24
+    {
+        [TestMethod()]
+        public void Init()
+        {
+            var f1 = new Fixed<Q8_24>(3);
+            Assert.AreEqual("3", f1.ToString());
+        }
+        [TestMethod()]
+        public void Add()
+        {
+            var f1 = new Fixed<Q8_24>(3);
+            var f2 = new Fixed<Q8_24>(2);
+            var f3 = f1.Add(f2);
+            Assert.AreEqual("5", f3.ToString());
+        }
+        [TestMethod()]
+        public void Subtract()
+        {
+            var f1 = new Fixed<Q8_24>(3);
+            var f2 = new Fixed<Q8_24>(2);
+            var f3 = f1.Subtract(f2);
+            Assert.AreEqual("1", f3.ToString());
+        }
+        [TestMethod()]
+        public void Multiply()
+        {
+            var f1 = new Fixed<Q8_24>(3);
+            var f2 = new Fixed<Q8_24>(2);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("6", f3.ToString());
+        }
+        [TestMethod()]
+        public void MultiplyHigher()
+        {
+            var f1 = new Fixed<Q8_24>(19);
+            var f2 = new Fixed<Q8_24>(13);
+            var f3 = f1.Multiply(f2);
+            Assert.AreEqual("-9", f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher()
+        {
+            var f1 = new Fixed<Q8_24>(248);
+            var f2 = new Fixed<Q8_24>(10);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)-0.799999952316284).ToString(), f3.ToString());
+        }
+        [TestMethod()]
+        public void DivideHigher2()
+        {
+            var f1 = new Fixed<Q8_24>(625);
+            var f2 = new Fixed<Q8_24>(1000);
+            var f3 = f1.Divide(f2);
+            Assert.AreEqual(((double)-4.70833331346512).ToString(), f3.ToString());
+        }
+
+    }
+}
--- LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
+++ LS2/Pokročilé programování pro .NET I/floatingPoint/floatingPoint/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("UnitTests")]
 
 namespace floatingPoint
 {
@@ -10,12 +12,99 @@ namespace floatingPoint
     {
         static void Main(string[] args)
         {
-            var f1 = new Fixed<Q24_8>(3);
-            var f2 = new Fixed<Q24_8>(2);
-            var f3 = f1.Multiply(f2);
-            Console.WriteLine($"a:{f1}, b={f2}, vysledek: {f3}");
+            Console.WriteLine("Q24_8:");
+            {
+                var f1 = new Fixed<Q24_8>(3);
+                Console.WriteLine($"3: {f1}");
 
+                var f2 = new Fixed<Q24_8>(2);
+                var f3 = f1.Add(f2);
+                Console.WriteLine($"3 + 2: {f3}");
 
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"3 * 2: {f3}");
+
+                f1 = new Fixed<Q24_8>(19);
+                f2 = new Fixed<Q24_8>(13);
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"19 * 13: {f3}");
+
+                f1 = new Fixed<Q24_8>(3);
+                f2 = new Fixed<Q24_8>(2);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"3 / 2: {f3}");
+
+                f1 = new Fixed<Q24_8>(248);
+                f2 = new Fixed<Q24_8>(10);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"248 / 10: {f3}");
+
+                f1 = new Fixed<Q24_8>(625);
+                f2 = new Fixed<Q24_8>(1000);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"625 / 1000: {f3}");
+            }
+
+            //
+
+            Console.WriteLine();
+            Console.WriteLine("Q16_16:");
+            {
+                var f1 = new Fixed<Q16_16>(3);
+                Console.WriteLine($"3: {f1}");
+
+                var f2 = new Fixed<Q16_16>(2);
+                var f3 = f1.Add(f2);
+                Console.WriteLine($"3 + 2: {f3}");
+
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"3 * 2: {f3}");
+
+                f1 = new Fixed<Q16_16>(19);
+                f2 = new Fixed<Q16_16>(13);
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"19 * 13: {f3}");
+
+                f1 = new Fixed<Q16_16>(248);
+                f2 = new Fixed<Q16_16>(10);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"248 / 10: {f3}");
+
+                f1 = new Fixed<Q16_16>(625);
+                f2 = new Fixed<Q16_16>(1000);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"625 / 1000: {f3}");
+            }
+
+            //
+            
+            Console.WriteLine();
+            Console.WriteLine("Q8_24:");
+            {
+                var f1 = new Fixed<Q8_24>(3);
+                var f2 = new Fixed<Q8_24>(2);
+                var f3 = f1.Add(f2);
+                Console.WriteLine($"3 + 2: {f3}");
+
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"3 * 2: {f3}");
+
+                f1 = new Fixed<Q8_24>(19);
+                f2 = new Fixed<Q8_24>(13);
+                f3 = f1.Multiply(f2);
+                Console.WriteLine($"19 * 13: {f3}");
+
+                f1 = new Fixed<Q8_24>(248);
+                f2 = new Fixed<Q8_24>(10);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"248 / 10: {f3}");
+
+                f1 = new Fixed<Q8_24>(625);
+                f2 = new Fixed<Q8_24>(1000);
+                f3 = f1.Divide(f2);
+                Console.WriteLine($"625 / 1000: {f3}");
+            }
+            
             Console.ReadKey();
         }
     }
@@ -46,7 +135,11 @@ namespace floatingPoint
         {
             return new Fixed<T>(precision.Multiply(floatingPointNumber, q.floatingPointNumber), true);
         }
-        public override string ToString()
+        public Fixed<T> Divide(Fixed<T> q)
+        {
+            return new Fixed<T>(precision.Divide(floatingPointNumber, q.floatingPointNumber), true);
+        }
+    public override string ToString()
         {
             return precision.ToString(floatingPointNumber);
         }
@@ -61,7 +154,8 @@ namespace floatingPoint
         {
             return a - b;
         }
-        public abstract int Multiply(int a, int b);        
+        public abstract int Multiply(int a, int b);
+        public abstract int Divide(int a, int b);
         public abstract string ToString(int floatingPointNumber);
         public abstract int Init(int floatingPointNumber);
     }
@@ -69,24 +163,18 @@ namespace floatingPoint
     {
         public override int Multiply(int a, int b)
         {
-            a = a & 0x00FF;
-            b = b & 0x00FF;
-            long c = a * b;
-
-
-            return (byte)(c >> 8);
+            long c = (long)a * (long)b;
+            return (int)(c >> 8);
+        }
+        public override int Divide(int a, int b)
+        {
+            long c = 0;
+            c = (long)(((long)a << 8) / ((long)b << 0));
+            return (int)(c >> 0);
         }
         public override string ToString(int n)
         {
-            byte f1 = (byte)(n >> 0);
-            byte i3 = (byte)(n >> 8);
-            byte i2 = (byte)(n >> 16);
-            byte i1 = (byte)(n >> 24);
-
-            if (f1 == 0)
-                return (i1 * 16 + i2 * 8 + i3).ToString();
-            else
-                return String.Format(i1 * 16 + i2 * 8 + i3 + "." + f1);
+            return ((double)(n) / Math.Pow(2, 8)).ToString();
         }
         public override int Init(int floatingPointNumber)
         {
@@ -97,28 +185,44 @@ namespace floatingPoint
     {
         public override int Multiply(int a, int b)
         {
-            a = a & 0x00FF;
-            b = b & 0x00FF;
-            long c = a * b;
-
-
-            return (byte)(c >> 16);
+            long c = (long)a * (long)b;
+            return (int)(c >> 16);
+        }
+        public override int Divide(int a, int b)
+        {
+            long c = 0;
+            c = (long)(((long)a << 16) / ((long)b << 0));
+            return (int)(c >> 0);
         }
         public override string ToString(int n)
         {
-            byte f2 = (byte)(n >> 0);
-            byte f1 = (byte)(n >> 8);
-            byte i1 = (byte)(n >> 16);
-            byte i2 = (byte)(n >> 24);
-
-            if (f1 == 0 && f2 == 0)
-                return (i2 * 8 + i1).ToString();
-            else
-                return String.Format(i2 * 8 + i1 + "." + f1 + f2);
+            return ((double)(n) / Math.Pow(2, 16)).ToString();
         }
         public override int Init(int floatingPointNumber)
         {
             return floatingPointNumber << 16;
+        }
+    }
+    class Q8_24 : Q
+    {
+        public override int Multiply(int a, int b)
+        {
+            long c = (long)a * (long)b;
+            return (int)(c >> 24);
+        }
+        public override int Divide(int a, int b)
+        {
+            long c = 0;
+            c = (long)(((long)a << 24) / ((long)b << 0));
+            return (int)(c >> 0);
+        }
+        public override string ToString(int n)
+        {
+            return ((double)(n) / Math.Pow(2, 24)).ToString();
+        }
+        public override int Init(int floatingPointNumber)
+        {
+            return floatingPointNumber << 24;
         }
     }
 }
