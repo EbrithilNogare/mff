87113c52b5e809c1a2521e2de4c3a41212e74ef1
David Nápravník <ebrithil@nogare.cz>
2019-04-25 17:09:06 +0200

    shaders repair

--- shaders/fragment/defered.glsl
+++ shaders/fragment/defered.glsl
@@ -9,18 +9,17 @@ struct light
 varying vec2 fragTexCoord;
 varying vec3 fragNormal;
 
+uniform sampler2D sampler;
+
 vec3 ambientLightIntensity 	= vec3(0.2, 0.2, 0.2);
 light sun 		= light(normalize(vec3( 0.0, 0.0,-1.0)), vec3(0.8, 0.8, 0.8));
-uniform sampler2D sampler;
 
 
 void main()
 {
-	vec3 surfaceNormal = normalize(fragNormal);
 	vec4 texture = texture2D(sampler, fragTexCoord);
 	vec3 lightIntensity =
 		ambientLightIntensity +
 		sun.color 	* max(dot(fragNormal, sun.direction	), 0.0);
 	gl_FragColor = vec4(texture.rgb * lightIntensity, texture.a);
-
 }
