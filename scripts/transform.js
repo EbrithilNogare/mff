77064adc34ec1d67e5fe659e8e4699ccea5bb595
David Nápravník <ebrithil@nogare.cz>
2019-05-15 10:28:39 +0200

    almost done transform

--- scripts/transform.js
+++ scripts/transform.js
@@ -4,7 +4,8 @@ let textures = [];		// Array of textures
 let programs = [];		// Array of programs
 let vertexArrays = [];	// Arrays of geometry data
 let particles = [];
-const NUM_PARTICLES = 1000;
+let currentSourceIdx = 0;
+const NUM_PARTICLES = 10000;
 
 try{
 	loadAsyncData();
@@ -58,22 +59,6 @@ function Init() {
 	}
 	if(!gl) throw `webgl not supported \n supported versions: \n ${webglVersions}`
 	
-
-
-
-
-
-
-	var ACCELERATION = -1.0;
-
-	appStartTime = Date.now();
-	currentSourceIdx = 0;
-
-
-
-
-
-	
 	programs["transform"] = gl.createProgram();
 	
 	/*
@@ -98,7 +83,7 @@ function Init() {
 		gl.attachShader(programs["transform"], fragmentShader);
 	}
 
-	var varyings = ['v_position', 'v_velocity', 'v_spawntime', 'v_lifetime'];
+	let varyings = ['v_position'];
 	gl.transformFeedbackVaryings(programs["transform"], varyings, gl.SEPARATE_ATTRIBS);
 
 	/*
@@ -114,42 +99,21 @@ function Init() {
 			throw `ERROR validating program! ${gl.getShaderInfoLog(programs["transform"])}`;
 		}
 	}
-
-
-
-
-
-
-
-
-
+	
 	// Get uniform locations for the draw program
 	drawTimeLocation = gl.getUniformLocation(programs["transform"], 'u_time');
-	var drawAccelerationLocation = gl.getUniformLocation(programs["transform"], 'u_acceleration');
+	let drawAccelerationLocation = gl.getUniformLocation(programs["transform"], 'u_acceleration');
 	
 	// -- Initialize particle data
 
-	var particlePositions = new Float32Array(NUM_PARTICLES * 2);
-	var particleVelocities = new Float32Array(NUM_PARTICLES * 2);
-	var particleSpawntime = new Float32Array(NUM_PARTICLES);
-	var particleLifetime = new Float32Array(NUM_PARTICLES);
-	var particleIDs = new Float32Array(NUM_PARTICLES);
+	let particlePositions = new Float32Array(NUM_PARTICLES * 2);
 
 	POSITION_LOCATION = 0;
-	VELOCITY_LOCATION = 1;
-	SPAWNTIME_LOCATION = 2;
-	LIFETIME_LOCATION = 3;
-	ID_LOCATION = 4;
-	NUM_LOCATIONS = 5;
+	NUM_LOCATIONS = 1;
 
-	for (var p = 0; p < NUM_PARTICLES; ++p) {
-		particlePositions[p * 2] = 0.0;
-		particlePositions[p * 2 + 1] = 0.0;
-		particleVelocities[p * 2] = 0.0;
-		particleVelocities[p * 2 + 1] = 0.0;
-		particleSpawntime[p] = 0.0;
-		particleLifetime[p] = 0.0;
-		particleIDs[p] = p;
+	for (let p = 0; p < NUM_PARTICLES; p++) {
+		particlePositions[p * 2] = Math.random()*2-1;
+		particlePositions[p * 2 + 1] = Math.random()*2-1;
 	}
 
 	// -- Init Vertex Arrays and Buffers
@@ -160,7 +124,7 @@ function Init() {
 
 	particleVBOs = new Array(particleVAOs.length);
 
-	for (var i = 0; i < particleVAOs.length; ++i) {
+	for (let i = 0; i < particleVAOs.length; ++i) {
 		particleVBOs[i] = new Array(NUM_LOCATIONS);
 
 		// Set up input
@@ -172,57 +136,17 @@ function Init() {
 		gl.vertexAttribPointer(POSITION_LOCATION, 2, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(POSITION_LOCATION);
 
-		particleVBOs[i][VELOCITY_LOCATION] = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, particleVBOs[i][VELOCITY_LOCATION]);
-		gl.bufferData(gl.ARRAY_BUFFER, particleVelocities, gl.STREAM_COPY);
-		gl.vertexAttribPointer(VELOCITY_LOCATION, 2, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(VELOCITY_LOCATION);
-
-		particleVBOs[i][SPAWNTIME_LOCATION] = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, particleVBOs[i][SPAWNTIME_LOCATION]);
-		gl.bufferData(gl.ARRAY_BUFFER, particleSpawntime, gl.STREAM_COPY);
-		gl.vertexAttribPointer(SPAWNTIME_LOCATION, 1, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(SPAWNTIME_LOCATION);
-
-		particleVBOs[i][LIFETIME_LOCATION] = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, particleVBOs[i][LIFETIME_LOCATION]);
-		gl.bufferData(gl.ARRAY_BUFFER, particleLifetime, gl.STREAM_COPY);
-		gl.vertexAttribPointer(LIFETIME_LOCATION, 1, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(LIFETIME_LOCATION);
-
-		particleVBOs[i][ID_LOCATION] = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, particleVBOs[i][ID_LOCATION]);
-		gl.bufferData(gl.ARRAY_BUFFER, particleIDs, gl.STATIC_READ);
-		gl.vertexAttribPointer(ID_LOCATION, 1, gl.FLOAT, false, 0, 0);
-		gl.enableVertexAttribArray(ID_LOCATION);
-
 		gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 		// Set up output
 		gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, particleTransformFeedbacks[i]);
 		gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, particleVBOs[i][POSITION_LOCATION]);
-		gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 1, particleVBOs[i][VELOCITY_LOCATION]);
-		gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 2, particleVBOs[i][SPAWNTIME_LOCATION]);
-		gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 3, particleVBOs[i][LIFETIME_LOCATION]);
-
 	}
 
 	gl.useProgram(programs["transform"]);
-	gl.uniform2f(drawAccelerationLocation, 0.0, ACCELERATION);
 
 	gl.enable(gl.BLEND);
 	gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-
-
-
-
-
-
-
-
-
-
-
 
 	/*
 	*	animation launch
@@ -235,30 +159,25 @@ function Init() {
 *	animation loop
 */
 function Loop() {
-	var time = Date.now() - appStartTime;
-	var destinationIdx = (currentSourceIdx + 1) % 2;
-
+	let destinationIdx = (currentSourceIdx + 1) % 2;
 
 	// Clear color buffer
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	// Toggle source and destination VBO
-	var sourceVAO = particleVAOs[currentSourceIdx];
-	var destinationTransformFeedback = particleTransformFeedbacks[destinationIdx];
+	let sourceVAO = particleVAOs[currentSourceIdx];
+	let destinationTransformFeedback = particleTransformFeedbacks[destinationIdx];
 
 	gl.bindVertexArray(sourceVAO);
 	gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, destinationTransformFeedback);
-
-	// Set uniforms
-	gl.uniform1f(drawTimeLocation, time);
-
+	
 	// Draw particles using transform feedback
 	gl.beginTransformFeedback(gl.POINTS);
 	gl.drawArrays(gl.POINTS, 0, NUM_PARTICLES);
 	gl.endTransformFeedback();
 
 	// Ping pong the buffers
-	currentSourceIdx = (currentSourceIdx + 1) % 2;
+	currentSourceIdx = !!!currentSourceIdx-0;
 
 	// go to next frame
 	requestAnimationFrame(Loop);
--- shaders/fragment/transform.glsl
+++ shaders/fragment/transform.glsl
@@ -1,10 +1,14 @@
 #version 300 es
 precision highp float;
-precision highp int;
+
+in vec2 v_position;
 
 out vec4 color;
 
 void main()
 {
-    color = vec4(0,0,1,1);
+    color = vec4(
+        v_position*0.5+0.5, //RG
+        0,                  //B
+        v_position.y+1.0);  //A
 }
