081ee1e32cf4885ad4cb8665dcdfa595ddeaad75
David Nápravník <david.napravnik@bimproject.cz>
2019-05-18 21:15:08 +0200

    c# paralizer done

--- LS2/Pokročilé programování pro .NET I/LibraryParallelQueriesSkeleton/MergeSortQuery/MergeSortQuery.cs
+++ LS2/Pokročilé programování pro .NET I/LibraryParallelQueriesSkeleton/MergeSortQuery/MergeSortQuery.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
 
@@ -11,17 +11,88 @@ namespace MergeSortQuery {
 	class MergeSortQuery {
 		public Library Library { get; set; }
 		public int ThreadCount { get; set; }
+        Sorter sorter = new Sorter();
 
-		public List<Copy> ExecuteQuery() {
-			if (ThreadCount == 0) throw new InvalidOperationException("Threads property not set and default value 0 is not valid.");
+        public List<Copy> ExecuteQuery() {
+            List<Copy> copies = new List<Copy>();
+            foreach (var item in Library.Copies)
+            {
+                if (item.State == CopyState.OnLoan && item.Book.Shelf[2] < 'R')
+                {
+                    copies.Add(item);
+                }
+            }
 
-			return new List<Copy>();
-		}
-	}
 
-	class ResultVisualizer {
+            MergeSorte(copies, (int)Math.Floor(Math.Log(ThreadCount, 2)), 0, copies.Count);
+            
+
+            return copies;
+        }
+        private void MergeSorte(List<Copy> copies, int depth, int start, int count)
+        {
+            if (depth<=0)
+            {
+                copies.Sort(start, count, sorter);
+            }
+            else
+            {
+                Task task1 = Task.Factory.StartNew(() => MergeSorte(copies, depth - 1, start, count / 2));
+                MergeSorte(copies, depth - 1, start + count / 2, count - count / 2);
+                Task.WaitAll(task1);
+
+                var newList = new List<Copy>();
+                newList.AddRange(copies.GetRange(start, count / 2));
+                newList.AddRange(copies.GetRange(start + count / 2, count - count / 2));
+
+                for (int i = 0; i < count; i++)
+                {
+                    copies[start + i] = newList[i];
+                }
+            }
+        }
+    }
+
+
+    class Sorter : IComparer<Copy>
+    {
+        public int Compare(Copy x, Copy y)
+        {
+            int result;
+            result = x.OnLoan.DueDate.CompareTo(y.OnLoan.DueDate);
+            if (result != 0)
+                return result;
+
+            result = x.OnLoan.Client.LastName.CompareTo(y.OnLoan.Client.LastName);
+            if (result != 0)
+                return result;
+
+            result = x.OnLoan.Client.FirstName.CompareTo(y.OnLoan.Client.FirstName);
+            if (result != 0)
+                return result;
+
+            result = x.Book.Shelf.CompareTo(y.Book.Shelf);
+            if (result != 0)
+                return result;
+
+            result = x.Id.CompareTo(y.Id);
+            if (result != 0)
+                return result;
+
+            return result;
+            
+        }
+    }
+
+
+    class ResultVisualizer {
 		public static void PrintCopy(StreamWriter writer, Copy c) {
-			writer.WriteLine("{0} {1}: {2} loaned to {3}, {4}.", c.OnLoan.DueDate.ToShortDateString(), c.Book.Shelf, c.Id, c.OnLoan.Client.LastName, System.Globalization.StringInfo.GetNextTextElement(c.OnLoan.Client.FirstName));
+			writer.WriteLine("{0} {1}: {2} loaned to {3}, {4}.",
+                c.OnLoan.DueDate.ToShortDateString(),
+                c.Book.Shelf,
+                c.Id,
+                c.OnLoan.Client.LastName,
+                System.Globalization.StringInfo.GetNextTextElement(c.OnLoan.Client.FirstName));
 		}
 	}
 }
--- LS2/Pokročilé programování pro .NET I/LibraryParallelQueriesSkeleton/MergeSortQuery/Program.cs
+++ LS2/Pokročilé programování pro .NET I/LibraryParallelQueriesSkeleton/MergeSortQuery/Program.cs
@@ -9,32 +9,37 @@ using LibraryModel;
 namespace MergeSortQuery {
 	class Program {
 		static void Main(string[] args) {
-
 			Library library = new Library();
 
-			// Requires ~ 800 MB of RAM
-			RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 / 4, ClientCount = 10000, LoanCount = 500000 * 5, MaxCopyCount = 10 };
+            // Requires ~ 10 MB of RAM
+            // RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2501000 / 16, ClientCount = 1000, LoanCount = 50 * 1000, MaxCopyCount = 10 };
 
-			// Requires ~ 1500 MB of RAM
-			// RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 / 2, ClientCount = 10000, LoanCount = 500000 * 5, MaxCopyCount = 10 };
+            // Requires ~ 800 MB of RAM
+             RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 / 4, ClientCount = 10000, LoanCount = 500000 * 5, MaxCopyCount = 10 };
 
-			// Requires ~ 5000 MB of RAM	
-			// RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 * 2, ClientCount = 10000, LoanCount = 500000 * 10, MaxCopyCount = 10 };
-			Console.WriteLine("Generating library content ...");
+            // Requires ~ 1500 MB of RAM
+            // RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 / 2, ClientCount = 10000, LoanCount = 500000 * 5, MaxCopyCount = 10 };
+
+            // Requires ~ 5000 MB of RAM	
+            // RandomGenerator rg = new RandomGenerator { RandomSeed = 42, BookCount = 2500000 * 2, ClientCount = 10000, LoanCount = 500000 * 10, MaxCopyCount = 10 };
+            Console.WriteLine("Generating library content ...");
 			rg.FillLibrary(library);
 
 			QueryTester.RunQuery("reference single threaded", ReferenceLibraryQueries.Queries.SingleThreadedQuery, ReferenceLibraryQueries.Queries.PrintCopy, library);
 
 			QueryTester.RunQuery("reference parallel", ReferenceLibraryQueries.Queries.ParallelQuery, ReferenceLibraryQueries.Queries.PrintCopy, library);
 
-			for (int threads = 1; threads <= 8; threads++) {
-				QueryTester.RunQuery(
-					string.Format("MergeSort {0:D2} threads", threads),
-					lib => new MergeSortQuery { Library = lib, ThreadCount = threads }.ExecuteQuery(),
-					ResultVisualizer.PrintCopy,
-					library
-				);
-			}
+            foreach (var threads in new int[] { 1,2,4,8})
+            {
+                QueryTester.RunQuery(
+                    string.Format("MergeSort {0:D2} threads", threads),
+                    lib => new MergeSortQuery { Library = lib, ThreadCount = threads }.ExecuteQuery(),
+                    ResultVisualizer.PrintCopy,
+                    library
+                );
+            }
+
+            Console.ReadKey();
 		}
 	}
 }
