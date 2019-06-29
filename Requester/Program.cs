0ef813c3a883d870019c7c1638f1155a4caa5345
David Napravnik <d@nogare.cz>
2019-06-29 18:27:19 +0200

    Requester init

---
+++ Requester/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Requester
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            NewMethod();
+            Console.ReadKey();
+        }
+
+        private static async void NewMethod()
+        {
+            var values = new Dictionary<string, string>
+                {
+                   { "thing1", "hello" },
+                   { "thing2", "world" }
+                };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(response);
+            Console.ReadKey();
+        }
+        
+    }
+
+    
+}
