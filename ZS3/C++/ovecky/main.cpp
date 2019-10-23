ef94dd34d398c533467156f3329f065857951dc1
David Napravnik <d@nogare.cz>
2019-10-23 22:03:21 +0200

    oveckyDU

--- ZS3/C++/ovecky/main.cpp
+++ ZS3/C++/ovecky/main.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include "main.h"
-#include "ovecky.cpp"
 
 
 int main()
 {
 	std::string data = GetLine();
-	
 	ovecky o;
-
 	std::cout << "znaku: " << o.CountChars(data) << "\n";
 	std::cout << "slov: " << o.CountWords(data) << "\n";
 	std::cout << "vet: " << o.CountSentences(data) << "\n";
 	std::cout << "radku: " << o.CountLines(data) << "\n";
-	std::cout << "pocet: " << o.CountNumbers(data) << "\n";
+	std::cout << "cisel: " << o.CountNumbers(data) << "\n";
 	std::cout << "soucet: " << o.SumOfNumbers(data) << "\n";
-	
+
 	return 0;
 }
 
 std::string GetLine() {
+	std::string allData;
 	std::string data;
-	std::getline(std::cin, data);
-	return data;
+	while (! std::cin.eof())
+	{
+		std::getline(std::cin, data);
+		allData += data + '\n';
+	}
+	return allData;
 }
---
+++ ZS3/C++/ovecky/main.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include "ovecky.h"
+
+std::string GetLine();
