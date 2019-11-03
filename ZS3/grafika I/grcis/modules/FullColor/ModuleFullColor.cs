9f206909f78fad379c43d7e0ae7230f02fcabdfd
David Napravnik <d@nogare.cz>
2019-11-04 00:03:18 +0100

    grafikaUkol121HalfWork

--- ZS3/grafika I/grcis/modules/FullColor/ModuleFullColor.cs
+++ ZS3/grafika I/grcis/modules/FullColor/ModuleFullColor.cs
@@ -7,277 +7,207 @@ using Utilities;
 
 namespace Modules
 {
-  public class ModuleFullColor : DefaultRasterModule
-  {
-    /// <summary>
-    /// Mandatory plain constructor.
-    /// </summary>
-    public ModuleFullColor ()
+    public class ModuleFullColor : DefaultRasterModule
     {
-      // Default cell size (wid x hei).
-      param = "wid=4096,hei=4096";
-    }
-
-    /// <summary>
-    /// Author's full name.
-    /// </summary>
-    public override string Author => "00pilot";
-
-    /// <summary>
-    /// Name of the module (short enough to fit inside a list-boxes, etc.).
-    /// </summary>
-    public override string Name => "FullColor";
-
-    /// <summary>
-    /// Tooltip for Param (text parameters).
-    /// </summary>
-    public override string Tooltip => "[wid=<width>][,hei=<height>][,slow][,ignore-input][,no-check]";
-
-    /// <summary>
-    /// Usually read-only, optionally writable (client is defining number of inputs).
-    /// </summary>
-    public override int InputSlots => 1;
-
-    /// <summary>
-    /// Usually read-only, optionally writable (client is defining number of outputs).
-    /// </summary>
-    public override int OutputSlots => 1;
-
-    /// <summary>
-    /// Input raster image.
-    /// </summary>
-    protected Bitmap inImage = null;
-
-    /// <summary>
-    /// Output raster image.
-    /// </summary>
-    protected Bitmap outImage = null;
-
-    /// <summary>
-    /// Output message (color check).
-    /// </summary>
-    protected string message;
-
-    /// <summary>
-    /// Assigns an input raster image to the given slot.
-    /// Doesn't start computation (see #Update for this).
-    /// </summary>
-    /// <param name="inputImage">Input raster image (can be null).</param>
-    /// <param name="slot">Slot number from 0 to InputSlots-1.</param>
-    public override void SetInput (
-      Bitmap inputImage,
-      int slot = 0)
-    {
-      inImage = inputImage;
-    }
-
-    /// <summary>
-    /// Recompute the output image[s] according to input image[s].
-    /// Blocking (synchronous) function.
-    /// #GetOutput() functions can be called after that.
-    /// </summary>
-    public override void Update ()
-    {
-      // Input image is optional.
-      // Starts a new computation.
-      UserBreak = false;
-
-      // Default values.
-      int wid = 4096;
-      int hei = 4096;
-      bool fast = true;
-      bool ignoreInput = false;
-      bool check = true;
-
-      // We are not using 'paramDirty', so the 'Param' string has to be parsed every time.
-      Dictionary<string, string> p = Util.ParseKeyValueList(param);
-      if (p.Count > 0)
-      {
-        // wid=<int> [image width in pixels]
-        if (Util.TryParse(p, "wid", ref wid))
-          wid = Math.Max(1, wid);
-
-        // hei=<int> [image height in pixels]
-        if (Util.TryParse(p, "hei", ref hei))
-          hei = Math.Max(1, wid);
-
-        // slow ... use Bitmap.SetPixel()
-        fast = !p.ContainsKey("slow");
-
-        // ignore-input ... ignore input image even if it is present
-        ignoreInput = p.ContainsKey("ignore-input");
-
-        // no-check ... disable color check at the end
-        check = !p.ContainsKey("no-check");
-      }
-
-      outImage = new Bitmap(wid, hei, PixelFormat.Format24bppRgb);
-
-      // Generate full-color image.
-      int xo, yo;
-      byte ro, go, bo;
-
-      if (!ignoreInput &&
-          inImage != null)
-      {
-        // Input image is present => use it.
-
-        // Convert pixel data (fast memory-mapped code).
-        PixelFormat iFormat = inImage.PixelFormat;
-        if (!PixelFormat.Format24bppRgb.Equals(iFormat) &&
-            !PixelFormat.Format32bppArgb.Equals(iFormat) &&
-            !PixelFormat.Format32bppPArgb.Equals(iFormat) &&
-            !PixelFormat.Format32bppRgb.Equals(iFormat))
-          iFormat = PixelFormat.Format24bppRgb;
-
-        int width  = inImage.Width;
-        int height = inImage.Height;
-        int xi, yi;
-        BitmapData dataIn  = inImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, iFormat);
-        BitmapData dataOut = outImage.LockBits(new Rectangle( 0, 0, wid, hei ), ImageLockMode.WriteOnly, outImage.PixelFormat);
-        unsafe
+        /// <summary>
+        /// Mandatory plain constructor.
+        /// </summary>
+        public ModuleFullColor()
         {
-          byte* iptr, optr;
-          byte ri, gi, bi;
-          int dI = Image.GetPixelFormatSize(iFormat) / 8;               // pixel size in bytes
-          int dO = Image.GetPixelFormatSize(outImage.PixelFormat) / 8;  // pixel size in bytes
+            // Default cell size (wid x hei).
+            param = "wid=4096,hei=4096";
+        }
 
-          yi = 0;
-          for (yo = 0; yo < hei; yo++)
-          {
-            // User break handling.
-            if (UserBreak)
-              break;
+        /// <summary>
+        /// Author's full name.
+        /// </summary>
+        public override string Author => "David Napravnik";
 
-            iptr = (byte*)dataIn.Scan0  + yi * dataIn.Stride;
-            optr = (byte*)dataOut.Scan0 + yo * dataOut.Stride;
+        /// <summary>
+        /// Name of the module (short enough to fit inside a list-boxes, etc.).
+        /// </summary>
+        public override string Name => "121";
 
-            xi = 0;
-            for (xo = 0; xo < wid; xo++)
+        /// <summary>
+        /// Tooltip for Param (text parameters).
+        /// </summary>
+        public override string Tooltip => "nothing to change";
+
+        /// <summary>
+        /// Usually read-only, optionally writable (client is defining number of inputs).
+        /// </summary>
+        public override int InputSlots => 1;
+
+        /// <summary>
+        /// Usually read-only, optionally writable (client is defining number of outputs).
+        /// </summary>
+        public override int OutputSlots => 1;
+
+        /// <summary>
+        /// Input raster image.
+        /// </summary>
+        protected Bitmap inImage = null;
+
+        /// <summary>
+        /// Output raster image.
+        /// </summary>
+        protected Bitmap outImage = null;
+
+        /// <summary>
+        /// Output message (color check).
+        /// </summary>
+        protected string message;
+
+        /// <summary>
+        /// Assigns an input raster image to the given slot.
+        /// Doesn't start computation (see #Update for this).
+        /// </summary>
+        /// <param name="inputImage">Input raster image (can be null).</param>
+        /// <param name="slot">Slot number from 0 to InputSlots-1.</param>
+        public override void SetInput(Bitmap inputImage, int slot = 0) { inImage = inputImage; }
+
+        /// <summary>
+        /// Recompute the output image[s] according to input image[s].
+        /// Blocking (synchronous) function.
+        /// #GetOutput() functions can be called after that.
+        /// </summary>
+        public override void Update()
+        {
+            // Input image is optional.
+            // Starts a new computation.
+            UserBreak = false;
+
+            // Default values.
+            int wid = 4096;
+            int hei = 4096;
+            bool check = true;
+            outImage = new Bitmap(wid, hei, PixelFormat.Format24bppRgb);
+            BitmapData dataOut = outImage.LockBits(new Rectangle(0, 0, wid, hei), ImageLockMode.WriteOnly, outImage.PixelFormat);
+
+            unsafe
             {
-              // read input colors
-              bi = iptr[0];
-              gi = iptr[1];
-              ri = iptr[2];
+                byte* iptr, optr, newOptr;
+                byte ri, gi, bi;
+                
+                int dO = Image.GetPixelFormatSize(outImage.PixelFormat) / 8;  // pixel size in bytes
 
-              // !!! TODO: do anything with the colors
-              bo = bi;
-              go = (byte)((gi + xo) & 0xFF);
-              ro = (byte)((ri + yo) & 0xFF);
+                //byte* baseIptr = (byte*)dataIn.Scan0;
+                byte* baseOptr = (byte*)dataOut.Scan0;
+                int blockSize = 16;
+                for (int x = 0; x < wid / blockSize; x++) // fill with all colors
+                {
+                    if (UserBreak) break;
+                    for (int y = 0; y < hei / blockSize; y++)
+                    {
+                        for (int xx = 0; xx < blockSize; xx++)
+                        {
+                            for (int yy = 0; yy < blockSize; yy++)
+                            {
+                                int picX = (x * blockSize + xx);
+                                int picY = (y * blockSize + yy);
+                                optr = baseOptr + (picX * wid + picY) * dO;
+                                optr[0] = (byte)x;
+                                optr[1] = (byte)y;
+                                optr[2] = (byte)(xx*blockSize+yy);
+                            }
+                        }
+                    }
+                }
+                if (inImage != null)
+                {
+                    PixelFormat iFormat;
+                    int width = 0;
+                    int height = 0;
+                    BitmapData dataIn = null;
+                    int dI;
+                    // Convert pixel data (fast memory-mapped code).
+                    iFormat = inImage.PixelFormat;
+                    if (!PixelFormat.Format24bppRgb.Equals(iFormat) &&
+                        !PixelFormat.Format32bppArgb.Equals(iFormat) &&
+                        !PixelFormat.Format32bppPArgb.Equals(iFormat) &&
+                        !PixelFormat.Format32bppRgb.Equals(iFormat))
+                        iFormat = PixelFormat.Format24bppRgb;
 
-              // write output colors
-              optr[0] = bo;
-              optr[1] = go;
-              optr[2] = ro;
+                    width = inImage.Width;
+                    height = inImage.Height;
+                    dataIn = inImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, iFormat);
+                    dI = Image.GetPixelFormatSize(iFormat) / 8;               // pixel size in bytes
 
-              iptr += dI;
-              optr += dO;
-              if (++xi >= width)
-              {
-                xi = 0;
-                iptr = (byte*)dataIn.Scan0 + yi * dataIn.Stride;
-              }
+                    // todo: do something here
+
+
+
+
+                    inImage.UnlockBits(dataIn);
+                }
+                else
+                {
+                    Random rand = new Random();
+                    blockSize = 512;
+                    for (int x = 0; x < wid; x += blockSize) // shuffle blocks of pixels
+                    {
+                        if (UserBreak) break;
+                        for (int y = 0; y < hei; y += blockSize)
+                        {
+                            for (int xx = 0; xx < blockSize; xx++)
+                            {
+                                for (int yy = 0; yy < blockSize; yy++)
+                                {
+                                    int randX = rand.Next(0, blockSize);
+                                    int randY = rand.Next(0, blockSize);
+                                    byte[] temp = new byte[3];
+
+                                    int picX = (x + xx);
+                                    int picY = (y + yy);
+                                    optr = baseOptr + (picX * wid + picY) * dO;
+
+                                    int newPicX = (x + randX);
+                                    int newPicY = (y + randY);
+                                    newOptr = baseOptr + (newPicX * wid + newPicY) * dO;
+
+                                    temp[0] = optr[0];
+                                    temp[1] = optr[1];
+                                    temp[2] = optr[2];
+
+                                    optr[0] = newOptr[0];
+                                    optr[1] = newOptr[1];
+                                    optr[2] = newOptr[2];
+
+                                    newOptr[0] = temp[0];
+                                    newOptr[1] = temp[1];
+                                    newOptr[2] = temp[2];
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
-            if (++yi >= height)
-              yi = 0;
-          }
-        }
-        outImage.UnlockBits(dataOut);
-        inImage.UnlockBits(dataIn);
-      }
-      else
-      {
-        // No input => generate constant full-color image.
+            outImage.UnlockBits(dataOut);            
 
-        int col;
-        if (fast)
-        {
-          // Generate pixel data (fast memory-mapped code).
-
-          BitmapData dataOut = outImage.LockBits(new Rectangle(0, 0, wid, hei), ImageLockMode.WriteOnly, outImage.PixelFormat);
-          unsafe
-          {
-            byte* optr;
-            int dO = Image.GetPixelFormatSize(outImage.PixelFormat) / 8;  // pixel size in bytes
-
-            col = 0;
-            for (yo = 0; yo < hei; yo++)
+            // Output message.
+            if (check &&
+                !UserBreak)
             {
-              // User break handling.
-              if (UserBreak)
-                break;
-
-              optr = (byte*)dataOut.Scan0 + yo * dataOut.Stride;
-
-              for (xo = 0; xo < wid; xo++, col++)
-              {
-                // !!! TODO: do anything with the input color
-                bo = (byte)((col >> 16) & 0xFF);
-                go = (byte)((col >> 8) & 0xFF);
-                ro = (byte)(col & 0xFF);
-
-                // write output colors
-                optr[0] = bo;
-                optr[1] = go;
-                optr[2] = ro;
-
-                optr += dO;
-              }
+                long colors = Draw.ColorNumber(outImage);
+                message = colors == (1 << 24) ? "Colors: 16M, Ok" : $"Colors: {colors}, Fail";
             }
-          }
-          outImage.UnlockBits(dataOut);
+            else
+                message = null;
         }
-        else
-        {
-          // Generate pixel data (slow mode).
+        
+        /// <summary>
+        /// Returns an output raster image.
+        /// Can return null.
+        /// </summary>
+        /// <param name="slot">Slot number from 0 to OutputSlots-1.</param>
+        public override Bitmap GetOutput(int slot = 0) => outImage;
 
-          col = 0;
-          for (yo = 0; yo < hei; yo++)
-          {
-            // User break handling.
-            if (UserBreak)
-              break;
-
-            for (xo = 0; xo < wid; xo++, col++)
-            {
-              // !!! TODO: do anything with the input color
-              bo = (byte)((col >> 16) & 0xFF);
-              go = (byte)((col >> 8) & 0xFF);
-              ro = (byte)(col & 0xFF);
-
-              // write output colors
-              outImage.SetPixel(xo, yo, Color.FromArgb(ro, go, bo));
-            }
-          }
-        }
-      }
-
-      // Output message.
-      if (check &&
-          !UserBreak)
-      {
-        long colors = Draw.ColorNumber(outImage);
-        message = colors == (1 << 24) ? "Colors: 16M, Ok" : $"Colors: {colors}, Fail";
-      }
-      else
-        message = null;
+        /// <summary>
+        /// Returns an optional output message.
+        /// Can return null.
+        /// </summary>
+        /// <param name="slot">Slot number from 0 to OutputSlots-1.</param>
+        public override string GetOutputMessage(int slot = 0) => message;
     }
-
-    /// <summary>
-    /// Returns an output raster image.
-    /// Can return null.
-    /// </summary>
-    /// <param name="slot">Slot number from 0 to OutputSlots-1.</param>
-    public override Bitmap GetOutput (
-      int slot = 0) => outImage;
-
-    /// <summary>
-    /// Returns an optional output message.
-    /// Can return null.
-    /// </summary>
-    /// <param name="slot">Slot number from 0 to OutputSlots-1.</param>
-    public override string GetOutputMessage (
-      int slot = 0) => message;
-  }
 }
