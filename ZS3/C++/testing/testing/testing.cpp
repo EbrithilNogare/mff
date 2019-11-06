2acf23d9c42c5223e5bf47ca6f2702b36f3227c4
David Napravnik <d@nogare.cz>
2019-11-06 15:34:05 +0000

    cppSort

--- ZS3/C++/testing/testing/testing.cpp
+++ ZS3/C++/testing/testing/testing.cpp
@@ -5,12 +5,65 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
+
+
+std::vector<std::pair<std::string, int>> database;
+
+void addFilm(std::string name, int date) {
+	database.push_back(std::pair<std::string, int>{name, date});
+}
+
+bool mySort(const std::pair<std::string, int>& s1, const std::pair<std::string, int>& s2) {
+	
+	if (s1.first < s2.first)
+		return true;
+	else if (s1.first > s2.first)
+		return false;
+	else
+		return (s1.second < s2.second);
+}
+
+void sortDatabase() {
+	std::sort(database.begin(), database.end(), mySort);
+}
+
+class std::pair<std::string, int> {
+	bool operator<(const std::pair<std::string, int>& y) {
+		if (this.first < y.first)
+			return true;
+		else if (this.first > y.first)
+			return false;
+		else
+			return (this.second< y.second);
+	}
+};
+
+void showDatabase() {
+	for (auto&& i : database)
+	{	
+		std::cout << i.first << ": "<< i.second << std::endl;
+	}
+}
+
 
 int main()
 {
-	// todo make some CUI
+
+	addFilm("nazev1", 1021);
+	addFilm("nazev4", 1014);
+	addFilm("nazev2", 1092);
+	addFilm("nazev3", 1043);
+	addFilm("nazev3", 1045);
+	addFilm("nazev3", 1044);
+
+	sortDatabase();
+
+	showDatabase();
 }
 
+
+/*	slovnik
 std::multimap<std::string, std::string> dictionary;
 
 void addTranslate(std::string *word, std::string *translation) {
@@ -45,3 +98,4 @@ std::vector<std::string> findTranslate(std::string *word) {
 
 	return toReturn;
 }
+*/
