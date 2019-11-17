f3e92b2fee5d2cafd0416cac408772eb01856acc
David Napravnik <d@nogare.cz>
2019-11-17 16:49:02 +0100

    c++HW1Working

--- ZS3/C++/hw1/automat.h
+++ ZS3/C++/hw1/automat.h
@@ -1,1 +1,37 @@
-int init();
+// automat.h
+// author : David Napravnik
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+class Automat{
+public:
+	enum symbol { number, letter, fence, space, other }; // [0-9], [a-zA-z], [#], [ ]
+	enum state { s0, s1, s2, s3, s4, s5, s6, er };
+
+	map<string, string> marcoList;
+	vector<char> wordStorage;
+	vector<char> macroStorage;
+	state currentState;
+	vector<vector<state>> automatDirections;
+
+	Automat();
+
+	symbol GetSymbolType(char inputSymbol);
+
+	void runAutomat(char inputSymbol);
+
+	void state0(char input);
+	void state1(char input);
+	void state2(char input);
+	void state3(char input);
+	void state4(char input);
+	void state5(char input);
+	void state6(char input);
+	void stateEr(char input);
+};
--- ZS3/C++/hw1/hw1.cpp
+++ ZS3/C++/hw1/hw1.cpp
@@ -9,10 +9,38 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+	Automat automat;
 
+	// todo add macros in parameters support
+	vector<string> arg(argv, argv + argc);
+	if (arg.size() > 1) {
+		automat.currentState = Automat::s2;
+		for (int i = 1; i < arg.size(); i++)
+		{
+			string word = arg[i];
+			for (int j = 0; j < word.length(); j++)
+			{
+				automat.runAutomat(word[j]);
+			}
+			automat.runAutomat(' ');
+		}
+		automat.runAutomat('#');
+		automat.runAutomat(' ');
+	}
 
+	istream& s = cin;
+	char c;
+	for (;;)
+	{
+		c = s.get();
+		if (s.fail()) break;
+		//cout << (c == '\n' ? '^':c) ;
+		automat.runAutomat(c);
+	}
+	if(automat.currentState == Automat::er)
+		cout << "Error" << "\n";
 }
 
 
