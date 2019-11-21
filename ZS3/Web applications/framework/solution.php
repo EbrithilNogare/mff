5a2ec366696cba6ff92178ba94bdc1587c401cfd
David Napravnik <d@nogare.cz>
2019-11-21 11:15:55 +0100

    web du php 02 init

---
+++ ZS3/Web applications/framework/solution.php
@@ -0,0 +1,15 @@
+<?php
+
+class ConfigPreprocessor
+{
+	public function __construct($config)
+	{
+	}
+
+	/**
+	 * Get an array of tasks from the config in the right order.
+	 */
+	public function getAllTasks()
+	{
+	}
+}
