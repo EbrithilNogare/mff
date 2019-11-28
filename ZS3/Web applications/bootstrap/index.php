ce6b89dbc0a5d464b0a3cdfc8e54a0d3539bf7ab
David Napravnik <d@nogare.cz>
2019-11-28 20:42:41 +0100

    php 03

--- ZS3/Web applications/bootstrap/index.php
+++ ZS3/Web applications/bootstrap/index.php
@@ -1,38 +1,54 @@
 <?php
 
-showHeader();
 showPage();
-showFooter();
 
 
-
-
-
-
-
-
-
-
-
-
-function showHeader(){
-	include "templates/_header.php";
-}
-
-function showFooter(){
-	include "templates/_footer.php";
-}
 
 function showPage(){
-	include "templates/".parsePageName();
+	$pageLink = getPage();
+	if(http_response_code()!=200){
+		echo http_response_code();
+		return;
+	}
+
+	if(!include "templates/_header.php")http_response_code(500);
+	incldeWithParams($pageLink);
+	if(!include "templates/_footer.php")http_response_code(500);
+}
+
+function getPage(){
+	$pageName = parsePageName();
+	$pageArguments = parseFileReturn($pageName);
+	foreach($pageArguments as $key => $value){
+		if(isset($_GET[$key]))
+			$$key = $_GET[$key];
+		else
+		http_response_code(400);
+	}
+
+	return $pageName;
+}
+
+function incldeWithParams($pageLink){
+	$pageArguments = parseFileReturn($pageLink);
+	foreach($pageArguments as $key => $value){
+		$$key = $_GET[$key];
+	}
+	if(!include "templates/".$pageLink)http_response_code(500);
+}
+
+function parseFileReturn($fileName){
+	if(file_exists("parameters/".$fileName) && !is_dir("parameters/".$fileName))
+		return include "parameters/".$fileName;
+	else
+		return [];
 }
 
 function parsePageName(){
-	$pageArgument = $_GET["page"];
+	$pageArgument = isset($_GET["page"])?$_GET["page"]:"";
 
 	if(!isPath($pageArgument) || $pageArgument===""){
 		http_response_code(400);
-		return "home/index.php"; // todo some beautiful default 400 page
 	}
 
 	if(file_exists("templates/".$pageArgument.".php")){
@@ -43,10 +59,7 @@ function parsePageName(){
 		else
 			return "home/index.php";
 	}
-
-
-	http_response_code(404); 
-	return "home/index.php"; // todo some beautiful default 404 page
+	http_response_code(404);
 }
 
 function isPath($input){	
