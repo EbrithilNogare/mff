82f5920e463d99355192bff87b5dae50b751bedf
David Napravnik <d@nogare.cz>
2019-12-23 02:33:40 +0100

    c++ hw2 parameters parse

---
+++ ZS3/C++/polysort/polysort.hpp
@@ -0,0 +1,20 @@
+// polysort.hpp
+// Author: David Napravnik
+
+#define DEBUG
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <map>
+#include <fstream>
+#include <vector>
+
+#ifndef polysort_H_
+#define polysort_H_
+
+int main(int argc, char* argv[]);
+bool parseParams(int argc, char** argv, std::string& paramI, std::string& paramO, char& paramS, std::vector<std::string>& paramRules);
+
+
+#endif // polysort_H_
