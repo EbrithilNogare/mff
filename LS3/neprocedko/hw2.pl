377e1c174242ead7c119e028ec7a6d053ff0ace7
David Napravnik <d@nogare.cz>
2020-03-13 00:29:18 +0100

    prolog hw2 almost done

---
+++ LS3/neprocedko/hw2.pl
@@ -0,0 +1,88 @@
+% 2. domácí úloha
+%
+% a) Implementujte predikát flat(+List, ?Result), který zploští libovolně
+% zanořený seznam seznamů List.
+%
+% flat([], R).
+% R = [].
+%
+% flat([[]], R).
+% R = [].
+%
+% flat([a,b,c], R).
+% R = [a,b,c].
+%
+% flat([a,[[],b,[]],[c,[d]]], R).
+% R = [a,b,c,d].
+%
+% Tento predikát měl být deterministický (speciálně otestujte, že po odmítnutí
+% neprodukuje duplikátní/nesprávné výsledky). Pokuste se o efektivní
+% implementaci pomocí akumulátoru.
+
+flat([], []).
+flat([Head | Body], Result) :-
+	flat(Head, FlatedHead),
+	flat(Body, FlatedBody),
+	push(FlatedHead, FlatedBody, Result),
+	!.
+flat(NotArray, [NotArray]).
+
+push([], X, X).
+push(X, [], X).
+push([L | M], R, [L | MR]) :- push( M, R, MR).
+
+
+% b) Implementuje predikát transp(+M, ?R), který transponuje matici M (uloženou
+% jako seznam seznamů). Pokud M není ve správném formátu (např. řádky mají
+% různé délky), dotaz transp(M, R) by měl selhat.
+%
+% transp([], R).
+% R = [].
+%
+% transp([[],[],[]], R).
+% R = [].
+%
+% transp([[a,b],[c,d],[e,f]], R).
+% R = [[a,c,e],[b,d,f]].
+%
+% transp([[a],[b,c],[d]], R).
+% false.
+
+transp([[] | _], []).
+transp(Matrix, [Row | Rows]) :-
+	getColumn(Matrix, Row, RestMatrix),
+	transp(RestMatrix, Rows).
+
+getColumn([], [], []).
+getColumn([[H | T] | Rows], [H | Hs], [T | Ts]) :-
+	getColumn(Rows, Hs, Ts).
+
+
+
+
+
+
+% c) (BONUSOVÁ ÚLOHA) Implementuje vkládání prvku pro AVL stromy.
+%
+% Použijte následující reprezentaci:
+% prázdný strom: nil
+% uzel: t(B,L,X,R) kde
+%   L je levý podstrom,
+%   X je uložený prvek,
+%   R je pravý podstrom,
+%   B je informace o vyvážení:
+%     B = l (levý podstrom je o 1 hlubší)
+%     B = 0 (oba podstromy jsou stejně hluboké)
+%     B = r (pravý podstrom je o 1 hlubší)
+%
+% avlInsert(+X, +T, -R)
+% X je vkládané číslo, T je strom před přidáním, R je strom po přidání
+%
+% avlInsert(1, nil, R).
+% R = t(0, nil, 1, nil).
+%
+% avlInsert(2, t(0, nil, 1, nil), R).
+% R = t(r, nil, 1, t(0, nil, 2, nil)).
+%
+% avlInsert(1, t(0, nil, 1, nil), R).
+% R = t(0, nil, 1, nil).
