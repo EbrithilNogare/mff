e58b7358d81b05dbd724fb57d5f04505f7dc6cda
David Napravnik <d@nogare.cz>
2020-03-21 23:32:46 +0100

    code refactory

--- ZS3/C++/unusualShadowMapping/light.h
+++ ZS3/C++/unusualShadowMapping/light.h
@@ -20,17 +20,50 @@ private:
 	bool visible = false; // todo to false
 	Model sphere = Model("../resources/models/sphere.obj", glm::vec3(0, 0, 0), glm::vec3(1.0f), glm::vec3(.2f));
 public:
+	glm::vec3 position;
 	unsigned int mapWidth = 1024;
 	unsigned int mapHeight = 1024;
-	Light(glm::vec3 position) {
-		sphere.setPosition(position);
+	float near_plane = 5.0f;
+	float far_plane = 20.0f;
+	unsigned int depthMapFBO;
+	unsigned int depthMap;
+	unsigned int colorMap;
+	glm::mat4 lightSpaceMatrix = glm::mat4(0);
+	Light(glm::vec3 initPosition) {
+		position = initPosition;
+
+		glGenTextures(1, &depthMap);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, mapWidth, mapHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		float borderColor[] = { 0.0, 0.0, 0.0, 1.0 };
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+
+		glGenFramebuffers(1, &depthMapFBO);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
 	void RenderHelper(Shader shader) {
 		if (visible) {
+			sphere.setPosition(position);
 			sphere.Draw(shader);
 		}
 	}
-	void setPosition(glm::vec3 position) {
-		sphere.setPosition(position);
+	void setPosition(glm::vec3 newPosition) {
+		position = newPosition;
+
+		glm::mat4 lightProjection, lightView;
+		lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)mapWidth / (GLfloat)mapHeight, near_plane, far_plane);
+
+		lightView = glm::lookAt(position, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+		lightSpaceMatrix = lightProjection * lightView;
 	}
 };
--- ZS3/C++/unusualShadowMapping/resources/shaders/debug_quad.fs
+++ ZS3/C++/unusualShadowMapping/resources/shaders/debug_quad.fs
@@ -11,12 +11,15 @@ uniform float far_plane;
 float LinearizeDepth(float depth)
 {
     float z = depth * 2.0 - 1.0; // Back to NDC 
-    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - z * (far_plane - near_plane));	
+    return (2.0 * near_plane * far_plane) / (far_plane + near_plane - z * (far_plane - near_plane)) / far_plane;	
 }
 
 void main()
 {             
-    float depthValue = texture(depthMap, TexCoords).r;
-    FragColor = vec4(vec3(LinearizeDepth(depthValue) / far_plane), 1.0); // perspective
-    // FragColor = vec4(vec3(depthValue), 1.0); // orthographic
+    vec4 shadowMap = texture(depthMap, TexCoords);
+	// <= switch    
+	FragColor = vec4(LinearizeDepth(shadowMap.r));
+    /*/
+	FragColor = vec4(shadowMap.r);
+	/**/
 }
