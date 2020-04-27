0a450f457ad2e7012188334c593611539d85dbf2
David Napravnik <d@nogare.cz>
2020-04-27 20:24:27 +0200

    finally some progress

--- LS3/neprocedko/hw5.hs
+++ LS3/neprocedko/hw5.hs
@@ -3,89 +3,47 @@
 -- https://blog.jle.im/entry/tries-with-recursion-schemes.html
 
 import qualified Data.Map as Map
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, isNothing)
 
--- 5. úloha
---
--- 1) Definujte datový typ 'Trie k v' reprezentující trii, kde klíče (řetězce)
--- jsou typu '[k]' a hodnoty typu 'v'.
 
-data Trie k v = Trie (Map.Map k (Trie k v)) (Maybe v) deriving (Eq, Read, Show)
+data Trie k v = Trie (Map.Map k (Trie k v)) (Maybe v) deriving (Eq, Read, Show, Ord)
+
 
 empty :: Trie k v
 empty = Trie Map.empty Nothing
 
---singleton :: [k] -> v -> Trie k v
---singleton k v = Trie { key = [(k, child)], value = Nothing }
---  where
---    child = Trie { key = [], value = (Just v) }
 
-
-
---insertWith :: (Ord k) => (v -> v -> v) -> [k] -> v -> Trie k v -> Trie k v
---insertWith f k v t
---  | t . key == k  = Trie { key = Just(k) , value = Just(v)}
---  | otherwise     = Trie { key = Just(k) , value = Just(v)}
+singleton :: Ord k => [k] -> v -> Trie k v
+singleton k v = insert k v empty
 
 
 insertWith :: Ord k => (v -> v -> v) -> [k] -> v -> Trie k v -> Trie k v
-insertWith f []     v (Trie nodes val) = Trie nodes (f v val)
-insertWith f (x:xs) v (Trie nodes val) = Trie (Map.alter (Just . insertWith f xs . fromMaybe empty) x nodes) (Just val)
+insertWith f []     v (Trie nodes val) = Trie nodes (Just v)
+insertWith f (x:xs) v (Trie nodes val) = Trie (Map.alter (Just . insertWith f xs v . fromMaybe empty) x nodes) val
 
 
-
--- 'insertWith f ks new t' vloží klíč 'ks' s hodnotou 'new' do trie 't'. Pokud
--- trie již klíč 'ks' (s hodnotou 'old') obsahuje, původní hodnota je nahrazena
--- hodnotou 'f new old'.
---
--- > insertWith (++) "a" "x" empty                  == fromList [("a","x")]
--- > insertWith (++) "a" "x" (fromList [("a","y")]) == fromList [("a","xy")]
---
-{-
 insert :: Ord k => [k] -> v -> Trie k v -> Trie k v
-insert []     v (Trie nodes val) = Trie nodes v
-insert (x:xs) v (Trie nodes val) = Trie (Map.alter (Just . insert xs . fromMaybe empty) x nodes) val
--}
--- 'insert ks new t' vloží klíč 'ks' s hodnotou 'new' do trie 't'. Pokud trie
--- již klíč 'ks' obsahuje, původní hodnota je nahrazena hodnotou 'new'
---
--- Hint: použijte 'insertWith'
---
--- > insert "a" "x" (fromList [("a","y")]) == fromList [("a","x")]
---
+insert []     v (Trie nodes val) = Trie nodes (Just v)
+insert (x:xs) v (Trie nodes val) = Trie (Map.alter (Just . insert xs v . fromMaybe empty) x nodes) val
+
 
 find :: (Ord k) => [k] -> Trie k v -> Maybe v
-find = undefined
+find []     (Trie _ v)     = v
+find (x:xs) (Trie nodes v) = fromMaybe v (find xs <$> Map.lookup x nodes)
 
--- 'find k t' vrátí hodnotu odpovídající klíči 'k' (jako 'Just v'), pokud
--- existuje, jinak 'Nothing'.
---
--- > find "a" empty                  == Nothing
--- > find "a" (fromList [("a","x")]) == Just "x"
---
 
-{-
 member :: Ord k => [k] -> Trie k v -> Bool
-member []     (Trie end _) = end
-member (x:xs) (Trie _ nodes) = fromMaybe False (member xs <$> Map.lookup x nodes)
--}
--- 'member k t' zjistí, jestli se klíč 'k' nalézá v trii 't'.
---
--- Hint: použijte 'find'
---
---
--- Funkce 'fromList' není nutná, ale může se vám hodit pro testování.
+member []     (Trie _ _)     = True
+member (x:xs) (Trie nodes _) = fromMaybe False (member xs <$> Map.lookup x nodes)
 
-{-
-fromList :: Ord k => [[(k, v)]] -> Trie k v
+
+fromList :: Ord k => [([k], v)] -> Trie k v
+fromList [] = empty
 fromList as = fromList' as empty
   where
-    fromList' []     trie = trie
-    fromList' (x:xs) trie = fromList' xs $ insert x trie
+    fromList' []          trie = trie
+    fromList' ((k, v):xs) trie = fromList' xs $ insert k v trie
 
--}
-
--- BONUS) Implementujte funkci
 
 delete :: (Ord k) => [k] -> Trie k v -> Trie k v
 delete = undefined
@@ -98,4 +56,13 @@ delete = undefined
 
 
 main = do
-  print ""
+  print "Tests:"
+  --print $ empty == fromList []
+  print $ singleton "a" "b" == fromList [("a", "b")]
+  print $ insertWith (++) "a" "x" empty == fromList [("a","x")]
+  print $ insertWith (++) "a" "x" (fromList [("a","y")]) == fromList [("a","xy")]
+  print $ insert "a" "x" (fromList [("a","y")]) == fromList [("a","x")]
+  --print $ find "a" empty == Nothing
+  print $ find "a" (fromList [("a","x")]) == Just "x"
+  print $ member "a" empty                  == False
+  print $ member "a" (fromList [("a","x")]) == True
