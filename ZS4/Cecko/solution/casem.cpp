3efd3ffcebf3952f56fb70d4f3879e417582a54e
David Nápravník <d@nogare.cz>
2020-12-05 01:23:30 +0100

    declarations init

--- ZS4/Cecko/solution/casem.cpp
+++ ZS4/Cecko/solution/casem.cpp
@@ -2,5 +2,32 @@
 
 namespace casem {
 
+	void DefineVariables(cecko::context* ctx, DeclarationSpecifiers specifiers, Declataros declarators){
+
+	}
+
+	std::vector<DeclarationSpecifiersDto> create_DeclarationSpecifiersDto(){
+		return std::vector<DeclarationSpecifiersDto>();
+	}
+	
+	std::vector<DeclaratorsDto> create_DeclaratorsDto(){
+		return std::vector<DeclaratorsDto>();
+	}
+
+	std::vector<DeclaratorsDto> create_PointersDto(){
+		return std::vector<PointersDto>();
+	}
+
+	CreateDeclarators();
+
+	cecko::CKTypeObs parse_etype(cecko::context* ctx, cecko::gt_etype etype){
+		switch(etype){
+			case cecko::gt_etype::BOOL: return ctx->get_bool_type();
+			case cecko::gt_etype::CHAR: return ctx->get_char_type();
+			case cecko::gt_etype::INT: return ctx->get_int_type();
+			default: exit(1)
+		}
+	}
+
 }
 
--- ZS4/Cecko/solution/casem.hpp
+++ ZS4/Cecko/solution/casem.hpp
@@ -7,6 +7,52 @@
 
 namespace casem {
 
+	class DeclarationSpecifiersDto {
+		public:
+		cecko::CKTypeObs type;
+		bool is_typedef;
+		bool is_const;
+		bool is_type;
+
+		DeclarationSpecifiersDto(void):
+			is_typedef(false), is_const(false), is_type(false), type() {}
+		
+		DeclarationSpecifiersDto(bool typdef, bool cnst):
+			is_typedef(typdef), is_const(cnst), is_type(false), type() {}
+		
+		DeclarationSpecifiersDto(cecko::CKTypeObs type):
+			is_typedef(false), is_const(false), is_type(true), type(type) {}
+	};
+
+	class DeclaratorDto{
+		public:
+			cecko::CIName identifier;
+			PointersDto poiters;
+			bool is_empty;
+			bool is_array;
+			bool is_function;
+			bool is_pointer;
+
+			DeclaratorDto(void):
+				is_empty(true), is_array(false), is_function(false), is_pointer(false), identifier(), pointers() {}
+
+			DeclaratorDto(cecko::CIName identifier):
+				identifier(identifier), is_empty(false) {}
+
+			void add_pointer(PointersDto& ptr) { pointers = ptr; is_pointer = true; }
+
+	}
+
+	class PointerDto {
+		public:
+			bool is_const;
+
+			PointerDto(bool is_const = false): 
+				is_const(is_const) {}
+	};
+
+	
+
 }
 
 #endif
