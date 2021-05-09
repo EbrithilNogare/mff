d124019023218c606b4c56e3ad966776393af9c8
David Napravnik <d@nogare.cz>
2021-05-09 22:13:20 +0200

    nprg mod and data concept

--- LS4/Linearni programovani/run tests.ps1
+++ LS4/Linearni programovani/run tests.ps1
@@ -1,8 +1,8 @@
 Get-ChildItem ".\tests" -Filter *.txt | 
 Foreach-Object {
 	$Now = Get-Date
-	cat $_.FullName |.\HW4.py > out.mod
-	$output = .\glpsol.exe -m ./out.mod | findstr "#OUTPUT: "
+	cat $_.FullName |.\HW4.py > coloring.data
+	$output = .\glpsol.exe -m ./coloring.mod -d ./coloring.data | findstr "#OUTPUT: "
 	$output = $output.Substring(9)
 	echo "$($_.Basename)`t$output`t$((NEW-TIMESPAN –Start $Now –End (Get-Date)).Seconds).$((NEW-TIMESPAN –Start $Now –End (Get-Date)).Milliseconds)s"
 }
