0da49b79b8f64173e578399f40f6e10bdb049f45
David Napravnik <d@nogare.cz>
2021-05-10 07:17:21 +0200

    nprg as fast as I can

--- LS4/Linearni programovani/HW4.py
+++ LS4/Linearni programovani/HW4.py
@@ -1,19 +1,24 @@
-import re
 from sys import stdin
 
 mode, nodes, lines = input().split()
 nodes = int(nodes)
 
-table = [[0 for c in range(nodes)] for r in range(nodes)]
-for line in stdin:
-	table[int(line.split()[0])][int(line.split()[2])] = 1
-
 print("data;")
 print("param N:=",nodes-1,";")
-print("set E :=")
 
-for row in range(len(table),-1,-1):
+table = [[1 for c in range(nodes)] for r in range(nodes)]
+
+for line in stdin:
+	table[int(line.split()[0])][int(line.split()[2])] = 0
+	table[int(line.split()[2])][int(line.split()[0])] = 0
+
+for i in range(len(table)):
+	table[i][i] = 0
+
+
+print("set E:=")
+for row in range(len(table)):
 	for column in range(row+1, len(table)):
-		if(table[row][column] == 0):
+		if(table[row][column] == 1):
 			print("("+str(row)+","+str(column)+")")
 print(";")
