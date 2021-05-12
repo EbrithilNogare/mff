3862ffee7eeadbe2818292da7f91da296176bc97
David Napravnik <d@nogare.cz>
2021-05-12 18:29:50 +0200

    LP version 2

--- LS4/Linearni programovani/run tests.ps1
+++ LS4/Linearni programovani/run tests.ps1
@@ -1,8 +1,24 @@
+$timeout = 300 # seconds
 Get-ChildItem ".\tests" -Filter *.txt | 
 Foreach-Object {
-	$Now = Get-Date
-	cat $_.FullName |.\HW4.py > coloring.data
-	$output = .\glpsol.exe -m ./coloring.mod -d ./coloring.data | findstr "#OUTPUT: "
-	$output = $output.Substring(9)
-	echo "$($_.Basename)`t$output`t$((NEW-TIMESPAN –Start $Now –End (Get-Date)).Seconds).$((NEW-TIMESPAN –Start $Now –End (Get-Date)).Milliseconds)s"
+	$job = Start-Job -ScriptBlock { 
+		$FullName = $($args[0])
+		$Now = Get-Date
+
+		cat $FullName | python .\HW4.py > coloring.data
+		$output = .\glpsol.exe -m ./coloring.mod -d ./coloring.data | findstr "#OUTPUT: "
+		
+		$output = $output.Substring(9)
+		$time = NEW-TIMESPAN –Start $Now –End (Get-Date)
+		echo "$output`t$($time.Seconds + 60*$time.Minutes).$($time.Milliseconds) s"
+	}  -ArgumentList $_.FullName
+	Wait-Job -Job $job -Timeout $timeout | Out-Null
+	Stop-Job -Job $job
+	$result = Receive-Job -Job $job
+	Remove-Job -Job $job
+	if([string]::IsNullOrEmpty($result)){
+		echo "$($_.Basename)`t?`t>$timeout s"
+	} else {
+		echo "$($_.Basename)`t$($result)"
+	}
 }
