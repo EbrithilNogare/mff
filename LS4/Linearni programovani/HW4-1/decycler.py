8172bb06d10cbaa581594f1312923f4a536ac98d
David Napravnik <d@nogare.cz>
2021-05-19 02:44:53 +0200

    LP HW4-1 done

--- LS4/Linearni programovani/HW4-1/decycler.py
+++ LS4/Linearni programovani/HW4-1/decycler.py
@@ -7,43 +7,46 @@ from sys import stdin
 nodes = int(input().split()[2])
 
 print("data;")
-print("param N:=",nodes-1,";")
+print("param N :=",nodes-1,";")
 
-edges = [[0 for c in range(nodes)] for r in range(nodes)]
-weights = [[0 for c in range(nodes)] for r in range(nodes)]
+graph = [[0 for c in range(nodes)] for r in range(nodes)]
 
 for line in stdin:
-	edges[int(line.split()[0])][int(line.split()[2])] = 1
-	weights[int(line.split()[0])][int(line.split()[2])] = int(line.split()[4][:-1])
+	graph[int(line.split()[0])][int(line.split()[2])] = int(line.split()[4][:-1])
 
-#for i in range(len(edges)):
-#	print(edges[i])
-#	
-#for i in range(len(weights)):
-#	print(weights[i])
 
-# odebrat vrcholy, ktere jsou budou jen ven, nebo jen dovnitr
-for row in range(len(edges)):
-	highestVertex = True
-	for column in range(len(edges)):
-		if(edges[row][column] == 1):
-			highestVertex = False
-	if(highestVertex):
-		for column in range(len(edges)):
-			edges[column][row] = 0
-		
+def isPartOfCycle(vertex1, vertex2):
+	if(graph[vertex1][vertex2] > 0):
+		for vertex3 in range(len(graph)):
+			if(graph[vertex2][vertex3] > 0):
+				if(graph[vertex3][vertex1] > 0):
+					return True
+				for vertex4 in range(len(graph)):
+					if(graph[vertex3][vertex4] > 0 and graph[vertex4][vertex1] > 0):
+						return True
+	return False
+
+for row in range(len(graph)):
+	for column in range(len(graph)):
+		if(not isPartOfCycle(row,column)):
+			graph[row][column] = 0
+
+
+
+#for i in range(len(graph)):
+#	print(graph[i])
 
 print("set NotEdges :=", end=" ")
-for row in range(len(edges)):
-	for column in range(len(edges)):
-		if(edges[row][column] == 0):
+for row in range(len(graph)):
+	for column in range(len(graph)):
+		if(graph[row][column] == 0):
 			print("("+str(row)+","+str(column)+")", end=" ")
 print(";")
 
 print("param W :"," ".join(str(x) for x in range(nodes)),":=")
-for row in range(len(edges)):
+for row in range(len(graph)):
 	print(row, end=" ")
-	for column in range(len(edges)):
-		print(weights[row][column], end=" ")
+	for column in range(len(graph)):
+		print(graph[row][column], end=" ")
 	print("")
 print(";")
