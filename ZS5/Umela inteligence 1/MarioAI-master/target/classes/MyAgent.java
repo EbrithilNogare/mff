fe959cc194ffe496b83fea6c558f53a238df7f55
David Napravnik <d@nogare.cz>
2021-10-14 13:45:42 +0200

    mario: 100 100 90 85 1

--- ZS5/Umela inteligence 1/MarioAI-master/target/classes/MyAgent.java
+++ ZS5/Umela inteligence 1/MarioAI-master/target/classes/MyAgent.java
@@ -5,24 +5,29 @@ import engine.core.LevelScene;
 import engine.graphics.VisualizationComponent;
 import engine.input.*;
 
-// Code your custom agent here!
-
 public class MyAgent extends MarioAIBase {
-	/**
-	 * An agent that sprints forward and jumps if it detects an obstacle ahead.
-	 * 
-	 * @author Jakub 'Jimmy' Gemrot, gemrot@gamedev.cuni.cz
-	 */
+	/*  
+		0 1 2 3 4 5
+	-2  . . . . . .
+	-1  . # . . . .
+	 0  M # # # . .
+	 1  . # # . . .
+	 2  . . . . . .
+	*/
 	private boolean enemyAhead() {
-		return     entities.danger(1, 0) || entities.danger(1, -1) 
-				|| entities.danger(2, 0) || entities.danger(2, -1)
-				|| entities.danger(3, 0) || entities.danger(2, -1);
+		return    
+			false ||
+									 entities.danger(1,-1) ||
+			entities.danger(0, 0) || entities.danger(1, 0) || entities.danger(2, 0) || entities.danger(3, 0) ||
+									 entities.danger(1, 1) || entities.danger(2, 1) ||
+			false
+		;
 	}
 	
 	private boolean brickAhead() {
-		return     tiles.brick(1, 0) || tiles.brick(1, -1) 
-				|| tiles.brick(2, 0) || tiles.brick(2, -1)
-				|| tiles.brick(3, 0) || tiles.brick(3, -1);
+		return     tiles.anyTile(1, 0) 
+				|| tiles.anyTile(2, 0)
+				|| tiles.anyTile(3, 0);
 	}
 
 	@Override
@@ -30,31 +35,28 @@ public class MyAgent extends MarioAIBase {
 		super.debugDraw(vis, level, env, g);
 		if (mario == null) return;
 
-		String progress = ""+(int)mario.sprite.mapX+" " + "#".repeat((int)mario.sprite.mapX/8);
-		progress += "-".repeat(256/8-(int)mario.sprite.mapX/8);
-		VisualizationComponent.drawStringDropShadow(g, progress, 0, 2, 2);
-
-		// EXAMPLE DEBUG VISUALIZATION
 		String debug = "Debug info: " + (int)mario.sprite.mapX;
 		VisualizationComponent.drawStringDropShadow(g, debug, 0, 26, 1);
 	}
 
-    // Called on each tick to find out what action(s) Mario should take.
 	@Override
 	public MarioInput actionSelectionAI() {
         MarioInput input = new MarioInput();
 
-		// ALWAYS RUN RIGHT
 		input.press(MarioKey.RIGHT);
-		
-		// ALWAYS SPEED RUN
-		input.press(MarioKey.SPEED);
-		
-		// IF (ENEMY || BRICK AHEAD) => JUMP
+
+		if(enemyAhead())
+			if (mario.mayShoot) {
+				if (!lastInput.isPressed(MarioKey.SPEED))
+					input.press(MarioKey.SPEED);    
+			} else{
+				input.release(MarioKey.RIGHT);
+				input.press(MarioKey.LEFT);
+			}
+
         if (mario.mayJump && (enemyAhead() || brickAhead()))
             input.press(MarioKey.JUMP);
 		
-		// Keep jumping to go as high as possible.
 		if (mario.isJumping()) {
 			input.press(MarioKey.JUMP);
 		}
