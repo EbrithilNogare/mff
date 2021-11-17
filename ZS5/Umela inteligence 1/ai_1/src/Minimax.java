ca9d6025d7dd295fadccd6798f9ba007aaec4dc9
David Napravnik <d@nogare.cz>
2021-11-17 12:57:30 +0100

    minimax pseudocode

--- ZS5/Umela inteligence 1/ai_1/src/Minimax.java
+++ ZS5/Umela inteligence 1/ai_1/src/Minimax.java
@@ -1,20 +1,48 @@
-
 import minimax.*;
 
-public class Minimax<S, A> implements Strategy<S, A> {
+public class Minimax < S, A > implements Strategy < S, A > {
+	HeuristicGame < S, A > game;
+	public Minimax(HeuristicGame < S, A > game, int limit) {
+		this.game = game;
+		minimaxAlg(game.initialState(000), limit, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, true);
+	}
 
-  public Minimax(HeuristicGame<S, A> game, int limit) {
-  
-    // Your implementation goes here.
-    
-  }
+	private void minimaxAlg(S state, int depth, double alpha, double beta, Boolean maximizingPlayer) {
+		if(depth == 0 || game.isDone(state))
+			return;
 
-  // method in Strategy interface
-  public A action(S state) {
+		double maxEval;
+		if(maximizingPlayer){
+			maxEval = Double.NEGATIVE_INFINITY;
+			
+			for(A action : game.actions(state)){
+				S newState = game.clone(state);
+				double eval = minimax(action, depth - 1, alpha, beta false)
+				
+				maxEval = max(maxEval, eval)
+				alpha = max(alpha, eval)
+				if beta <= alpha
+					break
+				return maxEval
+				
+		} else {
+			minEval = +infinity
+			for(A action : game.actions(state)){
+				eval = minimax(action, depth - 1, alpha, beta true)
+				minEval = min(minEval, eval)
+				beta = min(beta, eval)
+				if beta <= alpha
+					break
+			}
+			return minEval
+	}
+	
+	// method in Strategy interface
+	public A action(S state) {
 
-    // Your implementation goes here.
+		// Your implementation goes here.
 
-    return null;
-  }
+		return null;
+	}
 
 }
