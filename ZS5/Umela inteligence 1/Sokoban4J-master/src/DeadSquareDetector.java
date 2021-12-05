c9308053754786d5e66117f2e3036da40f53ec66
David Napravnik <d@nogare.cz>
2021-12-05 16:28:51 +0100

    fixed repeating code

--- ZS5/Umela inteligence 1/Sokoban4J-master/src/DeadSquareDetector.java
+++ ZS5/Umela inteligence 1/Sokoban4J-master/src/DeadSquareDetector.java
@@ -13,40 +13,38 @@ class DeadSquareDetector{
 
 		List<Pair<Integer,Integer>> goals = new ArrayList<Pair<Integer,Integer>>();
 
-		for (int x = 1; x < board.width()-1; x++) {
-			for (int y = 1; y < board.height()-1; y++) {
-				if(CTile.forSomeBox(board.tile(x,y))){
+		for (int x = 1; x < board.width()-1; x++)
+			for (int y = 1; y < board.height()-1; y++)
+				if(CTile.forSomeBox(board.tile(x,y)))
 					goals.add(new Pair<Integer,Integer>(x, y));
-				}
-			}
-		}
+				
 
-		for(Pair<Integer, Integer> goal : goals){
-			List<Pair<Integer, Integer>> toBeSearched = new ArrayList<Pair<Integer, Integer>>();
+		List<Pair<Integer, Integer>> toBeSearched = new ArrayList<Pair<Integer, Integer>>();
+		for(Pair<Integer, Integer> goal : goals)
 			toBeSearched.add(goal);
+		
 
-			while(toBeSearched.size() != 0){
-				int x = toBeSearched.get(0).first;
-				int y = toBeSearched.get(0).second;
-				if(!deaths[x][y]){
-					toBeSearched.remove(0);
-					continue;
-				}
-				deaths[x][y] = false;
-
-				// from left
-				if(x>1 && !CTile.isWall(board.tile(x-1,y)) && !CTile.isWall(board.tile(x-2,y)))
-					toBeSearched.add(new Pair<Integer, Integer>(x-1,y));
-				// from top
-				if(y>1 && !CTile.isWall(board.tile(x,y-1)) && !CTile.isWall(board.tile(x,y-2)))
-					toBeSearched.add(new Pair<Integer, Integer>(x,y-1));
-				// from right
-				if(x+2<board.width() && !CTile.isWall(board.tile(x+1,y)) && !CTile.isWall(board.tile(x+2,y)))
-					toBeSearched.add(new Pair<Integer, Integer>(x+1,y));
-				// from bottom
-				if(y+2<board.height() && !CTile.isWall(board.tile(x,y+1)) && !CTile.isWall(board.tile(x,y+2)))
-					toBeSearched.add(new Pair<Integer, Integer>(x,y+1));
+		while(toBeSearched.size() != 0){
+			int x = toBeSearched.get(0).first;
+			int y = toBeSearched.get(0).second;
+			if(!deaths[x][y]){
+				toBeSearched.remove(0);
+				continue;
 			}
+			deaths[x][y] = false;
+
+			// from left
+			if(x>1 && !CTile.isWall(board.tile(x-1,y)) && !CTile.isWall(board.tile(x-2,y)))
+				toBeSearched.add(new Pair<Integer, Integer>(x-1,y));
+			// from top
+			if(y>1 && !CTile.isWall(board.tile(x,y-1)) && !CTile.isWall(board.tile(x,y-2)))
+				toBeSearched.add(new Pair<Integer, Integer>(x,y-1));
+			// from right
+			if(x+2<board.width() && !CTile.isWall(board.tile(x+1,y)) && !CTile.isWall(board.tile(x+2,y)))
+				toBeSearched.add(new Pair<Integer, Integer>(x+1,y));
+			// from bottom
+			if(y+2<board.height() && !CTile.isWall(board.tile(x,y+1)) && !CTile.isWall(board.tile(x,y+2)))
+				toBeSearched.add(new Pair<Integer, Integer>(x,y+1));
 		}
 
 		return deaths;
