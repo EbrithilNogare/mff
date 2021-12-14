9ef2888386a7d1c5fbb89a4f1e47f9e1a6b7575c
David Napravnik <d@nogare.cz>
2021-12-14 19:19:28 +0100

    csp solver forward check done

--- ZS5/Umela inteligence 1/ai_1/src/Solver.java
+++ ZS5/Umela inteligence 1/ai_1/src/Solver.java
@@ -7,7 +7,46 @@ public class Solver {
     // Return a list of variables (if any) whose values were inferred.  If a contradiction
     // is found, return null.
     public List<Integer> forwardCheck(BooleanCSP csp) {
-        throw new Error("unimplemented");
+        List<Integer> toReturn = new ArrayList<Integer>();
+
+        while(!csp.unchecked.isEmpty()){
+            Constraint constraint = csp.unchecked.remove();
+            int realCount = 0;
+            List<Integer> missingVars = new ArrayList<Integer>();
+
+            for (int var : constraint.vars) {
+                if(csp.value[var] == null){
+                    missingVars.add(var);    
+                    continue;
+                }
+                realCount += csp.value[var] ? 1 : 0;
+            }
+
+            if(realCount > constraint.count)
+                return null;
+            
+            if(missingVars.size() == 0)
+                continue;
+
+            if(realCount == constraint.count){
+                for(int missingVar : missingVars){
+                    toReturn.add(missingVar);
+                    csp.set(missingVar, false);
+                }
+                continue;
+            }
+
+            if(realCount + missingVars.size() == constraint.count){
+                for(int missingVar : missingVars){
+                    toReturn.add(missingVar);
+                    csp.set(missingVar, true);
+                }
+                continue;
+            }
+
+        }
+
+        return toReturn;
     }
 
     // Find a solution to the given CSP using backtracking.  The solution will not include
