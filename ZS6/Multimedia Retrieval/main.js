bb4ab45e6c9f0f8e35482590e0a2a2574de4f00e
David Napravnik <d@nogare.cz>
2022-10-17 00:31:48 +0200

    improved project for midJourney

--- ZS6/Multimedia Retrieval/main.js
+++ ZS6/Multimedia Retrieval/main.js
@@ -1,23 +1,68 @@
 // expects variable data with json of midjourney chat export
 
-data = data.messages.map(({ content, attachments }) => ({
-  text: content.slice(2, content.lastIndexOf("**")),
-  url: attachments[0].url,
-}));
+const SEPARATOR_REGEXP = /[;, ]+/;
+
+const inputDOM = document.getElementById("input");
+
+if (data.messages) {
+  data = data.messages.map(({ content, attachments }) => ({
+    text: content
+      .slice(2, content.lastIndexOf("**"))
+      .split(SEPARATOR_REGEXP)
+      .filter((item) => item[0] !== "<")
+      .filter((item) => item[0] !== "-")
+      .filter((item) => item[0] !== "—")
+      .filter((item) => item.length < 32)
+      .filter((item) => item !== "")
+      .filter(onlyUnique),
+    url: attachments[0]?.url,
+  }));
+  console.log("dataset convert done: ", data);
+}
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
 
 function search() {
-  const text = document.getElementById("input").value;
-  const result = data.filter((item) => {
-    return item.text.includes(text);
-  });
+  const input = inputDOM.value.split(SEPARATOR_REGEXP);
+  const result = data
+    .map((item) => {
+      let found = 0;
+      input.forEach((word) => {
+        item.text.includes(word) ? (found += 1) : null;
+      });
+      return {
+        ...item,
+        match: found / Math.max(input.length, item.text.length),
+      };
+    })
+    .sort((itemA, itemB) => {
+      return itemB.match - itemA.match;
+    })
+    .slice(0, 100);
 
-  document.getElementById("info").innerText = "found: " + result.length;
-  if (result.length > 0) {
-    document.getElementById("result").innerText = result[0].text;
-    document.getElementById("img").src = "";
-    document.getElementById("img").src = result[0].url;
-  } else {
-    document.getElementById("result").innerText = "nothing found";
-    document.getElementById("img").src = "";
-  }
+  console.log("result: ", result);
+
+  if (result.length === 0) return;
+
+  document.getElementById("info").innerText =
+    "match: " + (result[0].match * 100).toFixed(2) + "%";
+  document.getElementById("result").innerHTML = result[0].text
+    .map((item) => (input.includes(item) ? `<b>${item}</b>` : item))
+    .join(", ");
+
+  const imgDOM = document.getElementById("img");
+
+  imgDOM.src = "";
+  imgDOM.src =
+    result[0].url.replace("cdn.discordapp.com", "media.discordapp.net") +
+    "?width=700&height=700";
 }
+
+inputDOM.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    search();
+  }
+});
