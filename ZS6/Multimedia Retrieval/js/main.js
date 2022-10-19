132053fe8bd75eb56d1a4e16202787d18e01eac7
David Napravnik <d@nogare.cz>
2022-10-19 21:11:39 +0200

    improved design of mid journey finder

--- ZS6/Multimedia Retrieval/js/main.js
+++ ZS6/Multimedia Retrieval/js/main.js
@@ -3,6 +3,7 @@
 const SEPARATOR_REGEXP = /[;, ]+/;
 
 const inputDOM = document.getElementById("input");
+let indexOfResult = 0;
 
 if (data.messages) {
   data = data.messages.map(({ content, attachments }) => ({
@@ -24,6 +25,20 @@ function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
+function prev() {
+  indexOfResult = Math.max(0, indexOfResult - 1);
+  search();
+}
+function next() {
+  indexOfResult++;
+  search();
+}
+
+function newSearch() {
+  indexOfResult = 0;
+  search();
+}
+
 function search() {
   const input = inputDOM.value.split(SEPARATOR_REGEXP);
   const result = data
@@ -39,34 +54,44 @@ function search() {
     })
     .sort((itemA, itemB) => {
       return itemB.match - itemA.match;
-    })
-    .slice(0, 100);
-
-  console.log("result: ", result);
+    });
 
   if (result.length === 0) return;
 
   document.getElementById("info").innerText =
-    "match: " + (result[0].match * 100).toFixed(2) + "%";
-  document.getElementById("result").innerHTML = result[0].text
+    "match: " + (result[indexOfResult].match * 100).toFixed(2) + "%";
+  document.getElementById("result").innerHTML = result[indexOfResult].text
     .map((item) =>
       input.includes(item) ? createChip(item, true) : createChip(item)
     )
-    .join("");
+    .join(" ");
 
   const imgDOM = document.getElementById("img");
+  const imgLDOM = document.getElementById("imgL");
+  const imgRDOM = document.getElementById("imgR");
 
   imgDOM.src = "";
-  imgDOM.src = result[0].url.replace(
+  imgDOM.src = result[indexOfResult].url.replace(
     "cdn.discordapp.com",
     "media.discordapp.net"
   ); // +"?width=700&height=700";
+
+  imgRDOM.src =
+    result[indexOfResult + 1].url.replace(
+      "cdn.discordapp.com",
+      "media.discordapp.net"
+    ) + "?width=64&height=64";
+  imgLDOM.src =
+    result[indexOfResult - 1].url.replace(
+      "cdn.discordapp.com",
+      "media.discordapp.net"
+    ) + "?width=64&height=64";
 }
 
 inputDOM.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    search();
+    newSearch();
   }
 });
 
