99395dad15e6867590573982ca203a730782d910
David Napravnik <david.napravnik@mews.com>
2023-01-31 16:07:26 +0100

    added synonyms

--- ZS6/Multimedia Retrieval/js/main.js
+++ ZS6/Multimedia Retrieval/js/main.js
@@ -45,69 +45,98 @@ function newSearch() {
 }
 
 function search() {
-  const input = inputDOM.value.split(SEPARATOR_REGEXP);
+    const input = inputDOM.value.split(SEPARATOR_REGEXP);
 
-  const inputPicture = findByImg
-    ? Object.values(
-        ctx.getImageData(0, 0, canvas.width, canvas.height).data
-      ).filter((item, index) => index % 4 !== 3)
-    : [];
+    const inputPicture = findByImg
+        ? Object.values(
+            ctx.getImageData(0, 0, canvas.width, canvas.height).data
+        ).filter((item, index) => index % 4 !== 3)
+        : [];
 
-  const result = data
-    .map((item) => {
-      let foundScore = 0;
-      input.forEach((word) => {
-        item.text.includes(word) ? (foundScore += 1) : null;
-      });
-      foundScore /= Math.max(Math.max(input.length, item.text.length), 1);
+    const synonyms = [];
 
-      if (findByImg) {
-        imgScore = item.smallImg
-          .map((a, i) => 1 - Math.sqrt(Math.abs(a - inputPicture[i])) / 16)
-          .reduce((b, c) => b + c, 0);
-        imgScore /= inputPicture.length;
-        foundScore = (foundScore + imgScore) / 2;
-      }
+    input.forEach((word) => {
+        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                return Promise.reject(response);
+            })
+            .then((syndata) => {
+                parseSynonyms(syndata, synonyms)
+            })
+            .finally(function () {
+                const result = data
+                    .map((item) => {
+                        let foundScore = 0;
+                        input.forEach((word) => {
+                            item.text.includes(word) ? (foundScore += 1) : null;
+                        });
+                        foundScore /= Math.max(Math.max(input.length, item.text.length), 1);
 
-      return {
-        ...item,
-        match: foundScore,
-      };
-    })
-    .filter((item) => item.match > 0)
-    .sort((itemA, itemB) => {
-      return itemB.match - itemA.match;
+                        let synonymScore = 0;
+                        synonyms.forEach((synonym) => {
+                            item.text.includes(synonym) ? (synonymScore += 1) : null;
+                        });
+                        synonymScore /= Math.max(Math.max(input.length, item.text.length), 1);
+                        foundScore = (foundScore + synonymScore);
+
+                        if (findByImg) {
+                            imgScore = item.smallImg
+                                .map((a, i) => 1 - Math.sqrt(Math.abs(a - inputPicture[i])) / 16)
+                                .reduce((b, c) => b + c, 0);
+                            imgScore /= inputPicture.length;
+                            foundScore = (foundScore + imgScore) / 2;
+                        }
+
+                        return {
+                            ...item,
+                            match: foundScore,
+                        };
+                    })
+                    .filter((item) => item.match > 0)
+                    .sort((itemA, itemB) => {
+                        return itemB.match - itemA.match;
+                    });
+
+                if (result.length === 0) return;
+
+                document.getElementById("info").innerText =
+                    "match: " + (result[indexOfResult].match * 100).toFixed(2) + "%";
+                document.getElementById("result").innerHTML = result[indexOfResult].text
+                    .map((item) =>
+                        input.includes(item) ? createChip(item, true) : createChip(item)
+                    )
+                    .join(" ");
+                document.getElementById("synonyms").innerHTML =
+                    "synonyms: " + synonyms
+                        .map((item) =>
+                            result[indexOfResult].text.includes(item) ? createChip(item, true, true) : createChip(item, false, true)
+                        )
+                        .join(" ");
+
+                const imgDOM = document.getElementById("img");
+                const imgLDOM = document.getElementById("imgL");
+                const imgRDOM = document.getElementById("imgR");
+
+                imgDOM.src = "";
+                imgDOM.src =
+                    result[indexOfResult]?.url.replace(
+                        "cdn.discordapp.com",
+                        "media.discordapp.net"
+                    ) ?? ""; // +"?width=700&height=700";
+
+                imgRDOM.src =
+                    result[indexOfResult + 1]?.url
+                        .replace("cdn.discordapp.com", "media.discordapp.net")
+                        .concat("?width=128&height=128") ?? "";
+                imgLDOM.src =
+                    result[indexOfResult - 1]?.url
+                        .replace("cdn.discordapp.com", "media.discordapp.net")
+                        .concat("?width=128&height=128") ?? "";
+            });
     });
-
-  if (result.length === 0) return;
-
-  document.getElementById("info").innerText =
-    "match: " + (result[indexOfResult].match * 100).toFixed(2) + "%";
-  document.getElementById("result").innerHTML = result[indexOfResult].text
-    .map((item) =>
-      input.includes(item) ? createChip(item, true) : createChip(item)
-    )
-    .join(" ");
-
-  const imgDOM = document.getElementById("img");
-  const imgLDOM = document.getElementById("imgL");
-  const imgRDOM = document.getElementById("imgR");
-
-  imgDOM.src = "";
-  imgDOM.src =
-    result[indexOfResult]?.url.replace(
-      "cdn.discordapp.com",
-      "media.discordapp.net"
-    ) ?? ""; // +"?width=700&height=700";
-
-  imgRDOM.src =
-    result[indexOfResult + 1]?.url
-      .replace("cdn.discordapp.com", "media.discordapp.net")
-      .concat("?width=128&height=128") ?? "";
-  imgLDOM.src =
-    result[indexOfResult - 1]?.url
-      .replace("cdn.discordapp.com", "media.discordapp.net")
-      .concat("?width=128&height=128") ?? "";
 }
 
 inputDOM.addEventListener("keypress", function (event) {
@@ -117,10 +146,25 @@ inputDOM.addEventListener("keypress", function (event) {
   }
 });
 
-function createChip(text, bold = false) {
+function parseSynonyms(syndata, synonyms) {
+    syndata.forEach((result) => { // slo by nahradit .reduce, nebo nejakou obyc flattern funkci...
+        for (key in result) {
+            if (key == 'meanings') {
+                result[key].forEach((meaning) => {
+                    for (var i = 0; i < meaning.synonyms.length; i++) {
+                        if (synonyms.indexOf(meaning.synonyms[i]) == -1) synonyms.push(meaning.synonyms[i]); // aby nebyly duplicity
+                    }
+                })
+            }
+        }
+    })
+}
+
+function createChip(text, bold = false, syn = false) {
   return `<div class="chip ${
     bold ? "green lighten-1" : "orange lighten-2"
-  }">${text}</div>`;
+      }">${text}
+${syn ? '</div>' : '<i class="close material-icons">close</i></div>'}`;
 }
 
 function init() {
