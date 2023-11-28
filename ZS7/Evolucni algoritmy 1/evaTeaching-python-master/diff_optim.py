c9990a0ab5924694bfe597012d42cdae883952de
David Napravnik <ebrithilnogare@gmail.com>
2023-11-28 13:48:14 +0100

    version 0

--- ZS7/Evolucni algoritmy 1/evaTeaching-python-master/diff_optim.py
+++ ZS7/Evolucni algoritmy 1/evaTeaching-python-master/diff_optim.py
@@ -6,15 +6,15 @@ import math
 import co_functions as cf
 import utils
 
-DIMENSION = 10 # dimension of the problems
-POP_SIZE = 100 # population size
-MAX_GEN = 500 # maximum number of generations
-CX_PROB = 0.8 # crossover probability
-MUT_PROB = 0.2 # mutation probability
-MUT_STEP = 0.5 # size of the mutation steps
-REPEATS = 10 # number of runs of algorithm (should be at least 10)
-OUT_DIR = 'differential' # output directory for logs
-EXP_ID = 'default' # the ID of this experiment (used to create log names)
+DIMENSION = 10  # dimension of the problems
+POP_SIZE = 100  # population size
+MAX_GEN = 500  # maximum number of generations
+CX_PROB = 0.8  # crossover probability
+MUT_PROB = 0.2  # mutation probability
+MUT_STEP = 0.5  # size of the mutation steps
+REPEATS = 10  # number of runs of the algorithm (should be at least 10)
+OUT_DIR = 'differential'  # output directory for logs
+EXP_ID = 'default'  # the ID of this experiment (used to create log names)
 TOURNAMENT_COMPETITOR_COUNT = 2
 
 # creates the individual
@@ -25,14 +25,14 @@ def create_ind(ind_len):
 def create_pop(pop_size, create_individual):
     return [create_individual() for _ in range(pop_size)]
 
-# the tournament selection (roulette wheell would not work, because we can have 
+# the tournament selection (roulette wheel would not work because we can have
 # negative fitness)
 def tournament_selection(pop, fits, _k):
-     selected = []
-     for _ in range(len(pop)):
+    selected = []
+    for _ in range(len(pop)):
         parents = [random.randrange(0, len(pop)) for _ in range(TOURNAMENT_COMPETITOR_COUNT)]
-        selected.append(pop[max(parents, key=lambda ind:fits[ind])])
-     return selected
+        selected.append(pop[max(parents, key=lambda ind: fits[ind])])
+    return selected
 
 # implements the one-point crossover of two individuals
 def one_pt_cross(p1, p2, generation):
@@ -41,13 +41,14 @@ def one_pt_cross(p1, p2, generation):
     o2 = np.append(p2[:point], p1[point:])
     return o1, o2
 
-# implements the one-point crossover of two individuals
+# implements the arithmetic crossover of two individuals
 def arithmetic_cross(p1, p2, generation):
     alpha = random.random()
     o1 = alpha * p1 + (1 - alpha) * p2
     o2 = alpha * p2 + (1 - alpha) * p1
     return o1, o2
 
+# implements the simulated binary crossover of two individuals
 def simulated_binary_crossover(p1, p2, generation):
     eta = 20 + generation
     o1 = p1[::]
@@ -63,40 +64,29 @@ def simulated_binary_crossover(p1, p2, generation):
         o2[i] = 0.5 * (((1 - beta) * x1) + ((1 + beta) * x2))
     return o1, o2
 
-# gaussian mutation - we need a class because we want to change the step
-# size of the mutation adaptively
-class Mutation:
+# differential evolution operator with adaptive parameters
+def differential_evolution(pop, _fits, generation, F=None, CR=None):
+    if F is None:
+        F = 0.8 + 0.2 * (1.0 - generation / MAX_GEN)  # Adaptive F
+    if CR is None:
+        CR = 0.9 - 0.5 * (1.0 - generation / MAX_GEN)  # Adaptive CR
 
-    def __init__(self, step_size):
-        self.step_size = step_size
-
-    def __call__(self, ind, pop, generation):
-        return ind + self.step_size*np.random.normal(size=ind.shape)
-    
-    def __call__2(self, ind, pop, generation):
-        random_ind = pop[np.random.randint(0, len(pop))]
-        direction_vector = random_ind - ind
-        if np.all(direction_vector == 0):
-            direction_vector = np.random.normal(size=ind.shape)
-        direction_vector /= np.linalg.norm(direction_vector)
-        return ind + self.step_size * direction_vector
-
-
-
-# applies a list of genetic operators (functions with 1 argument - population) 
-# to the population
-def mate(pop, operators, generation):
-    for o in operators:
-        pop = o(pop, generation=generation)
-    return pop
+    new_pop = []
+    for target in pop:
+        candidates = random.sample(pop, 5)  # Use 5 individuals for mutation
+        a, b, c, d, e = candidates
+        mutant = a + F * (b - c) + F * (d - e)
+        trial = np.array([x if random.random() < CR else y for x, y in zip(mutant, target)])
+        new_pop.append(trial)
+    return new_pop
 
 # applies the cross function (implementing the crossover of two individuals)
 # to the whole population (with probability cx_prob)
-def crossover(pop, cross, cx_prob, generation):
+def crossover_operator(pop, cross_op, fits_objs, generation, cx_prob):  # Rename cross to cross_op
     off = []
     for p1, p2 in zip(pop[0::2], pop[1::2]):
         if random.random() < cx_prob:
-            o1, o2 = cross(p1, p2, generation=generation)
+            o1, o2 = cross_op(p1, p2, generation=generation, fits_objs=fits_objs)  # Rename cross to cross_op
         else:
             o1, o2 = p1[:], p2[:]
         off.append(o1)
@@ -108,20 +98,48 @@ def crossover(pop, cross, cx_prob, generation):
 def mutation(pop, mutate, mut_prob, generation):
     return [mutate(p, pop, generation) if random.random() < mut_prob else p[:] for p in pop]
 
+# gaussian mutation - we need a class because we want to change the step
+# size of the mutation adaptively
+class Mutation:
+
+    def __init__(self, step_size):
+        self.step_size = step_size
+
+    def __call__(self, ind, pop, generation):
+        return ind + self.step_size * np.random.normal(size=ind.shape)
+
+    def __call__2(self, ind, pop, generation):
+        random_ind = pop[np.random.randint(0, len(pop))]
+        direction_vector = random_ind - ind
+        if np.all(direction_vector == 0):
+            direction_vector = np.random.normal(size=ind.shape)
+        direction_vector /= np.linalg.norm(direction_vector)
+        return ind + self.step_size * direction_vector
+
+# applies a list of genetic operators (functions with 1 argument - population)
+# to the population
+def mate(pop, operators, generation, fits_objs):
+    for o in operators:
+        if o.__name__ == 'crossover_operator':
+            pop = o(pop, crossover_operator, fits_objs, generation=generation)  # Pass fits_objs to crossover_operator
+        else:
+            pop = o(pop, fits_objs, generation=generation)  # Pass fits_objs to other operators
+    return pop
+
 # implements the evolutionary algorithm
 # arguments:
 #   pop_size  - the initial population
-#   max_gen   - maximum number of generation
-#   fitness   - fitness function (takes individual as argument and returns 
+#   max_gen   - maximum number of generations
+#   fitness   - fitness function (takes individual as argument and returns
 #               FitObjPair)
-#   operators - list of genetic operators (functions with one arguments - 
+#   operators - list of genetic operators (functions with one argument -
 #               population; returning a population)
-#   mate_sel  - mating selection (funtion with three arguments - population, 
-#               fitness values, number of individuals to select; returning the 
+#   mate_sel  - mating selection (function with three arguments - population,
+#               fitness values, number of individuals to select; returning the
 #               selected population)
-#   mutate_ind - reference to the class to mutate an individual - can be used to 
+#   mutate_ind - reference to the class to mutate an individual - can be used to
 #               change the mutation step adaptively
-#   map_fn    - function to use to map fitness evaluation over the whole 
+#   map_fn    - function to use to map fitness evaluation over the whole
 #               population (default `map`)
 #   log       - a utils.Log structure to log the evolution run
 def evolutionary_algorithm(pop, max_gen, fitness, operators, mate_sel, mutate_ind, *, map_fn=map, log=None):
@@ -135,25 +153,13 @@ def evolutionary_algorithm(pop, max_gen, fitness, operators, mate_sel, mutate_in
         objs = [f.objective for f in fits_objs]
 
         mating_pool = mate_sel(pop, fits, POP_SIZE)
-        offspring = mate(mating_pool, operators, G)
+        offspring = mate(mating_pool, operators, G, fits_objs)  # Pass fits_objs to mate function
         pop = offspring[:]
 
     return pop
 
-# Differential Evolution Operator
-def differential_evolution(pop, _, generation, F=0.8, CR=0.9):
-    new_pop = []
-    for target in pop:
-        a, b, c = random.sample(pop, 3)
-        mutant = a + F * (b - c)
-        trial = np.array([x if random.random() < CR else y for x, y in zip(mutant, target)])
-        new_pop.append(trial)
-    return new_pop
-
-
 if __name__ == '__main__':
-
-    # use `functool.partial` to create fix some arguments of the functions 
+    # use `functool.partial` to create fix some arguments of the functions
     # and create functions with required signatures
     cr_ind = functools.partial(create_ind, ind_len=DIMENSION)
     # we will run the experiment on a number of different functions
@@ -163,31 +169,31 @@ if __name__ == '__main__':
                       cf.make_f08_rosenbrock,
                       cf.make_f10_rotated_ellipsoidal]
     fit_names = ['f02']
-    #fit_names = ['f01', 'f02', 'f06', 'f08', 'f10']
+    # fit_names = ['f01', 'f02', 'f06', 'f08', 'f10']
 
     for fit_gen, fit_name in zip(fit_generators, fit_names):
         fit = fit_gen(DIMENSION)
         mutate_ind = Mutation(step_size=MUT_STEP)
-        xover = functools.partial(crossover, cross=arithmetic_cross, cx_prob=CX_PROB)
+        xover = functools.partial(crossover_operator, cross=arithmetic_cross, cx_prob=CX_PROB)
         mut = functools.partial(mutation, mut_prob=MUT_PROB, mutate=mutate_ind)
 
-        # run the algorithm `REPEATS` times and remember the best solutions from 
+        # run the algorithm `REPEATS` times and remember the best solutions from
         # last generations
-    
         best_inds = []
         for run in range(REPEATS):
             # initialize the log structure
-            log = utils.Log(OUT_DIR, EXP_ID + '.' + fit_name , run, 
+            log = utils.Log(OUT_DIR, EXP_ID + '.' + fit_name, run,
                             write_immediately=True, print_frequency=5)
             # create population
             pop = create_pop(POP_SIZE, cr_ind)
             # run evolution - notice we use the pool.map as the map_fn
-            pop = evolutionary_algorithm(pop, MAX_GEN, fit, [xover, mut], tournament_selection, mutate_ind, map_fn=map, log=log)
+            pop = evolutionary_algorithm(pop, MAX_GEN, fit, [differential_evolution, xover, mut],
+                                          tournament_selection, mutate_ind, map_fn=map, log=log)
             # remember the best individual from last generation, save it to file
             bi = max(pop, key=fit)
             best_inds.append(bi)
-            
-            # if we used write_immediately = False, we would need to save the 
+
+            # if we used write_immediately = False, we would need to save the
             # files now
             # log.write_files()
 
