3c2385fd7bd4f5ca77924d6ead576e0cb1d3e1a0
David Napravnik <ebrithilnogare@gmail.com>
2024-02-01 19:18:02 +0100

    mcts for chess done

---
+++ ZS7/Artificial Intelligence for Computer Games/MCTSNode.cs
@@ -0,0 +1,143 @@
+﻿using Chess;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.MCTS_Testing
+{
+    public class MCTSNode
+    {
+        public Move move;
+        public MCTSNode parent;
+        public List<MCTSNode> childrens;
+        public int visits;
+        public float score;
+
+        public MCTSNode(Move move, MCTSNode parent)
+        {
+            this.move = move;
+            this.parent = parent;
+            this.childrens = new List<MCTSNode>();
+            this.visits = 0;
+            this.score = 0;
+        }
+
+        public MCTSNode Select(Board state, bool paritySource, bool chearringToWhite)
+        {
+            var node = this;
+            for (bool parity = paritySource; node.childrens.Count > 0; parity = !parity)
+            {
+                var bestChild = node.childrens[0];
+                var bestValue = float.MinValue;
+                foreach (var child in node.childrens)
+                {
+                    float uctValue = child.UCT(state.WhiteToMove != chearringToWhite);
+                    if (uctValue > bestValue)
+                    {
+                        bestChild = child;
+                        bestValue = uctValue;
+                    }
+                }
+                node = bestChild;
+                state.MakeMove(node.move);
+            }
+            return node;
+        }
+
+        public MCTSNode Expand(Board state, MoveGenerator moveGenerator)
+        {
+            MCTSNode child = null;
+            bool isRoot = parent == null;
+
+            foreach (var move in moveGenerator.GenerateMoves(state, isRoot))
+            {
+                if (state.KingSquare[0] == move.TargetSquare || state.KingSquare[1] == move.TargetSquare)
+                {
+                    childrens.Clear();
+                    return this;
+                }
+                child = new MCTSNode(move, this);
+                childrens.Insert(0, child);
+            }
+            if (child == null) return this;
+
+            state.MakeMove(child.move);
+
+            return child;
+        }
+
+        public float Simulate(Board state, Evaluation evaluation, MoveGenerator moveGenerator, System.Random random, int maxPlayoutDepth, bool chearringToWhite)
+        {
+            var simulatedState = state.GetLightweightClone();
+            bool whiteToMove = state.WhiteToMove;
+            for (int i = 0; i < maxPlayoutDepth; i++)
+            {
+                List<SimMove> possibleMoves = moveGenerator.GetSimMoves(simulatedState, whiteToMove);
+
+                if (possibleMoves.Count == 0)
+                {
+                    break;
+                }
+                SimMove nextMove = possibleMoves[random.Next(possibleMoves.Count)];
+
+                // check end of game
+                if (simulatedState[nextMove.endCoord1, nextMove.endCoord2]?.type == SimPieceType.King)
+                {
+                    return whiteToMove == chearringToWhite ? 1 : 0;
+                }
+
+                AlterBoardWithMove(simulatedState, nextMove);
+
+                whiteToMove = !whiteToMove;
+            }
+
+            switch (IsWinningState(simulatedState))
+            {
+                case 1:
+                    return chearringToWhite ? 1 : 0;
+                case 2:
+                    return chearringToWhite ? 0 : 1;
+            }
+            return evaluation.EvaluateSimBoard(simulatedState, chearringToWhite);
+        }
+
+        public void Backpropagate(float diff)
+        {
+            for (var node = this; node != null; node = node.parent)
+            {
+                node.score += diff;
+                node.visits++;
+            }
+        }
+
+        public float UCT(bool asEnemy = false)
+        {
+            if (visits == 0)
+            {
+                return float.MaxValue;
+            }
+            float exploitationValue = asEnemy ? 1 - score / visits : score / visits;
+            float c = 1; // Mathf.Sqrt(2);
+            float explorationValue = Mathf.Sqrt(Mathf.Log(parent.visits) / visits);
+            return exploitationValue + c * explorationValue;
+        }
+
+        private void AlterBoardWithMove(SimPiece[,] board, SimMove move)
+        {
+            board[move.endCoord1, move.endCoord2] = board[move.startCoord1, move.startCoord2];
+            board[move.startCoord1, move.startCoord2] = null;
+        }
+
+        // return 1 of white is winning, 2 if black and 0 or 3 if none
+        private byte IsWinningState(SimPiece[,] board)
+        {
+            byte toReturn = 0;
+            for (int x = 0; x < 8; x++)
+                for (int y = 0; y < 8; y++)
+                {
+                    if (board[x, y] != null && board[x, y].type == SimPieceType.King)
+                        toReturn += board[x, y].team ? (byte)1 : (byte)2;
+                }
+            return toReturn;
+        }
+    }
+}
---
+++ ZS7/Artificial Intelligence for Computer Games/MCTSSearch.cs
@@ -0,0 +1,167 @@
+﻿using Assets.Scripts.MCTS_Testing;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    class MCTSSearch : ISearch
+    {
+        public event System.Action<Move> onSearchComplete;
+
+        MoveGenerator moveGenerator;
+
+        float bestScore;
+        Move bestMove;
+        bool abortSearch;
+
+        MCTSSettings settings;
+        Board board;
+        Evaluation evaluation;
+
+        System.Random rand;
+
+        // Diagnostics
+        public SearchDiagnostics Diagnostics { get; set; }
+        System.Diagnostics.Stopwatch searchStopwatch;
+
+        public MCTSSearch(Board board, MCTSSettings settings)
+        {
+            this.board = board;
+            this.settings = settings;
+            evaluation = new Evaluation();
+            moveGenerator = new MoveGenerator();
+            rand = new System.Random();
+        }
+
+        public void StartSearch()
+        {
+            InitDebugInfo();
+
+            // Initialize search settings
+            bestMove = Move.InvalidMove;
+
+            moveGenerator.promotionsToGenerate = settings.promotionsToSearch;
+            abortSearch = false;
+            Diagnostics = new SearchDiagnostics();
+
+            MCTSNode root = new MCTSNode(new Move(), null);
+
+            SearchMoves(root);
+
+            onSearchComplete?.Invoke(bestMove);
+
+            if (!settings.useThreading)
+            {
+                LogDebugInfo();
+            }
+        }
+
+        public void EndSearch()
+        {
+            if (settings.useTimeLimit)
+            {
+                abortSearch = true;
+            }
+        }
+
+        void SearchMoves(MCTSNode root)
+        {
+            var maxNumOfPlayouts = !settings.limitNumOfPlayouts || settings.maxNumOfPlayouts <= 0 ? 100 * 1000 : settings.maxNumOfPlayouts;
+            var random = new System.Random(42);
+            for (int numOfPlayouts = 0; numOfPlayouts < maxNumOfPlayouts && !abortSearch; numOfPlayouts++)
+            {
+                // Setup
+                Board state = board.Clone();
+
+                // Selection
+                MCTSNode selected = root.Select(state, state.WhiteToMove == board.WhiteToMove, board.WhiteToMove);
+
+                // Expansion
+                selected = selected.Expand(state, moveGenerator);
+
+                // Simulation
+                int maxPlayoutDepth = settings.playoutDepthLimit > 0 ? settings.playoutDepthLimit : 100;
+                maxPlayoutDepth -= getDepth(selected) - 1;
+                float diff = selected.Simulate(state, evaluation, moveGenerator, random, maxPlayoutDepth, board.WhiteToMove);
+
+                // Backpropagation
+                selected.Backpropagate(diff);
+
+                // Diagnostics
+                Diagnostics.numPositionsEvaluated = numOfPlayouts + 1;
+            }
+
+            // Return best so far
+            ReturnBestSoFar(root);
+
+            // Diagnostics
+            Diagnostics.move = bestMove.Name;
+            Diagnostics.moveVal = bestScore.ToString();
+
+            /**/ // switch of debug
+            StringBuilder sb = new StringBuilder("- Tree: \n");
+            ExportTree(root, sb, 2);
+            UnityEngine.Debug.Log(sb.ToString());
+            UnityEngine.Debug.Log("selected node: " + bestMove.Name);
+            /**/
+        }
+
+        private int getDepth(MCTSNode selected)
+        {
+            int depth = 0;
+            while (selected != null)
+            {
+                depth++;
+                selected = selected.parent;
+            }
+            return depth;
+        }
+
+        void ReturnBestSoFar(MCTSNode root)
+        {
+            bestScore = float.MinValue;
+            foreach (var child in root.childrens)
+            {
+                if (child.visits > 0 && child.score / child.visits > bestScore)
+                {
+                    bestScore = child.score / child.visits;
+                    bestMove = child.move;
+                }
+            }
+        }
+
+        void LogDebugInfo()
+        {
+            // Optional
+            // Debug.Log("Search time: " + searchStopwatch.ElapsedMilliseconds + "ms");
+        }
+
+        void InitDebugInfo()
+        {
+            searchStopwatch = System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        // Data format for: https://vgarciasc.github.io/tree-renderer/
+        void ExportTree(MCTSNode node, StringBuilder sb, int depth)
+        {
+            List<MCTSNode> nodes = new List<MCTSNode>(node.childrens);
+            nodes.Sort((MCTSNode a, MCTSNode b) => { return (a.score / a.visits) - (b.score / b.visits) > 0 ? 1 : (a.score / a.visits) - (b.score / b.visits) < 0 ? -1 : 0; });
+            foreach (var child in nodes)
+            {
+                if (child.UCT() == float.MaxValue)
+                    continue;
+
+                sb.Append(string.Format("{0} Move: {1}; Score/Visits: {2,5}/{3,5}={4,5} Score: {5,5}\n",
+                    new string('-', depth),
+                    child.move.Name,
+                    child.score,
+                    child.visits,
+                    child.score / child.visits,
+                    child.UCT()
+                ));
+                if (child.move.Name == "d5-c6" || child.move.Name == "f7-e6" || child.move.Name == "d5-e6")
+                    ExportTree(child, sb, depth + 1);
+            }
+        }
+    }
+}
